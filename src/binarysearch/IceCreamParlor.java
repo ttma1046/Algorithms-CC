c3fa1f20b786f217738b4b7294755eee771bbbbe
Chandler Fang <chandlerfang@gmail.com>
2018-08-03 21:44:39 +1000

    Ice cream Parlor and Paths.

--- src/binarysearch/IceCreamParlor.java
+++ src/binarysearch/IceCreamParlor.java
@@ -2,6 +2,61 @@ package binarysearch;
 
 import java.util.Arrays;
 
+/*
+Each time Sunny and Johnny take a trip to the Ice Cream Parlor, they pool their money to buy ice cream. On any given day, the parlor offers a line of flavors. Each flavor has a cost associated with it.
+
+Given the value of money and the cost of each flavor for  trips to the Ice Cream Parlor, help Sunny and Johnny choose two distinct flavors such that they spend their entire pool of money during each visit. ID numbers are the 1- based index number associated with a . For each trip to the parlor, print the ID numbers for the two types of ice cream that Sunny and Johnny purchase as two space-separated integers on a new line. You must print the smaller ID first and the larger ID second.
+
+For example, there are  flavors having . Together they have  to spend. They would purchase flavor ID's  and  for a cost of . Use  based indexing for your response.
+
+Note:
+
+Two ice creams having unique IDs  and  may have the same cost (i.e., ).
+There will always be a unique solution.
+Function Description
+
+Complete the function whatFlavors in the editor below. It must determine the two flavors they will purchase and print them as two space-separated integers on a line.
+
+whatFlavors has the following parameter(s):
+
+cost: an array of integers representing price for a flavor
+money: an integer representing the amount of money they have to spend
+Input Format
+
+The first line contains an integer, , the number of trips to the ice cream parlor.
+
+Each of the next  sets of  lines is as follows:
+
+The first line contains .
+The second line contains an integer, , the size of the array .
+The third line contains  space-separated integers denoting the .
+Constraints
+
+Output Format
+
+Print two space-separated integers denoting the respective indices for the two distinct flavors they choose to purchase in ascending order. Recall that each ice cream flavor has a unique ID number in the inclusive range from  to .
+
+Sample Input
+
+2
+4
+5
+1 4 5 3 2
+4
+4
+2 2 4 3
+Sample Output
+
+1 4
+1 2
+Explanation
+
+Sunny and Johnny make the following two trips to the parlor:
+
+The first time, they pool together  dollars. There are five flavors available that day and flavors  and  have a total cost of .
+The second time, they pool together  dollars. There are four flavors available that day and flavors  and  have a total cost of .
+ */
+
 public class IceCreamParlor {
     public static int indexOf(int[] menu, int value, int excludeThis) {
         for (int i = 0; i < menu.length; i++) {
---
+++ src/resursion/Paths.java
@@ -0,0 +1,73 @@
+package resursion;
+
+public class Paths {
+    public static int countPathsR(int steps) {
+        if (steps < 0) {
+            return 0;
+        } else if (steps == 0) {
+            return 1;
+        }
+        return countPathsR(steps - 1) + countPathsR(steps - 2) + countPathsR(steps - 1);
+    }
+
+    public static int countPathsMemo(int steps) {
+        if (steps < 0) {
+            return 0;
+        }
+        return countPathsMemo(steps, new int[steps + 1]);
+    }
+
+    public static int countPathsMemo(int steps, int[] memo) {
+        if (steps < 0) {
+            return 0;
+        } else if (steps == 0) {
+            return 1;
+        }
+        if (memo[steps] == 0) {
+            memo[steps] = countPathsMemo(steps - 1, memo) + countPathsMemo(steps - 2, memo) + countPathsMemo(steps - 3, memo);
+        }
+
+        return memo[steps];
+    }
+
+    public static int countPathsDP(int steps) {
+        if (steps < 0) {
+            return 0;
+        } else if (steps <= 1 ) {
+            return 1;
+        }
+        int[] paths = new int[steps + 1];
+        paths[0] = 1;
+        paths[1] = 1;
+        paths[2] = 2;
+        for (int i = 3; i <= steps; i++) {
+            paths[i] = paths[i - 1] + paths[i - 2] + paths[i - 3];
+        }
+
+        return paths[steps];
+    }
+
+
+
+    public static int countPathsO(int steps) {
+        if (steps < 0) {
+            return 0;
+        } else if (steps <= 1 ) {
+            return 1;
+        }
+        int[] paths = {1, 1, 2};
+
+        for (int i = 3; i <= steps; i++) {
+            int count = paths[2] + paths[1] + paths[0];
+            paths[0] = paths[1];
+            paths[1] = paths[2];
+            paths[2] = count;
+        }
+
+        return paths[2];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
