ab1595423098b0d412820ec9d230a64835c4c3e4
Chandler Fang <chandlerfang@gmail.com>
2019-06-10 13:10:51 +1000

    Fix.

--- src/dp/Fib.java
+++ src/dp/Fib.java
@@ -1,10 +1,12 @@
 package dp;
 
 public class Fib {
-    int fib(int n) {
+    static int fib(int n) {
         if (n <= 0) {
             return 0;
         } else if (n == 1) {
+            return 0;
+        } else if (n == 2) {
             return 1;
         } else {
             return fib(n - 1) + fib(n - 2);
@@ -16,16 +18,45 @@ public class Fib {
             return 0;
         } else if (n == 1) {
             return 1;
-        } else if (mem[n] > 0) { // if (exist(mem, n))
-            return mem[n];
+        }  else if (mem[n - 1] > 0) { // if (exist(mem, n))
+            return mem[n - 1];
         }
 
         int f = fib(n - 1, mem) + fib( n - 2, mem);
         mem[n] = f;
         return f;
     } // O(N)
+
+    public static void main(String[] args) {
+        System.out.println(fib(6));
+    }
 }
 
+class Program {
+    public static int getNthFib(int n) {
+        int mem [] = new int[n];
+
+        return getNthFibWithMem(n - 1, mem);
+    }
+
+    private static int getNthFibWithMem(int n, int [] mem) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (mem[n] > 0) {
+            return mem[n];
+        }
+
+        int f = getNthFibWithMem(n - 1, mem) + getNthFibWithMem(n - 2, mem);
+        mem[n] = f;
+        return f;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getNthFib(6));
+    }
+}
 /*
                                                        fib(7)
 
