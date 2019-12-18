c4549bb98d5e048b07e83ee339a34473b6dcd04e
Chandler Fang <Chandler.Fang@servcorp.com.au>
2019-12-18 13:25:44 +1100

    Fair Candy Swap 888.

---
+++ src/array/FairCandySwap_888.java
@@ -0,0 +1,9 @@
+class FairCandySwap_888 {
+    public int[] fairCandySwap(int[] A, int[] B) {
+        if (A.length <= 0 || A == null || B.length <= 0 || B == null) {
+
+        }
+
+        return null;
+    }
+}
