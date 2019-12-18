aec44b5a7c70057b158d522c366859bb15e0ffbe
Chandler Fang <Chandler.Fang@servcorp.com.au>
2019-12-18 16:56:27 +1100

    Fair Candy Swap 888.

--- src/array/FairCandySwap_888.java
+++ src/array/FairCandySwap_888.java
@@ -1,9 +1,21 @@
 class FairCandySwap_888 {
     public int[] fairCandySwap(int[] A, int[] B) {
-        if (A.length <= 0 || A == null || B.length <= 0 || B == null) {
+        if (A.length <= 0 || A == null || B.length <= 0 || B == null) return null;
+        int [] result = new int[2];
+        
+        int sumA = 0;
+        int sumB = 0;
 
+        for (int i = 0, j = 0; i < A.length, j < B.length; i++, j++) {
+            sumA += A[i];
+            sumB += B[j];
+
+            if (Math.abs(sumA - sumB) / 2 == (A[i] - B[j])) {
+                result.add(A[i]);
+                result.add(B[j]);
+            }
         }
-
-        return null;
+        
+        return result;
     }
 }
