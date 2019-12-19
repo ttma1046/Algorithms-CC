75e425aae6353fee43a08ecd611a858ca0583381
Chandler Fang <Chandler.Fang@servcorp.com.au>
2019-12-19 11:23:30 +1100

    FairCandySwap_888

--- src/array/FairCandySwap_888.java
+++ src/array/FairCandySwap_888.java
@@ -1,4 +1,38 @@
 class FairCandySwap_888 {
+    /*
+    A - x + y = B - y + x;
+    2y = B - A - 2x
+    2x = A - B + 2y
+    x = (A - B) / 2 + y
+    y = (B - A) / 2 - x;
+    */
+
+    A-y+x = B-x+y
+    x = (B - A)/2 + y
+
+    public int[] FairCandySwap(int[] A, int[] B) {
+        if (A.length <= 0 || B.length <=0) return new int[0];
+        int sumA = 0, sumB = 0;
+        for (int x: A) { sumA += x; }
+        for (int x: B) { sumB += x; }
+        int delta = (sumA - sumB) / 2;
+        Set<Integer> myset = new HashSet<Integer>();
+
+        for (int x: A) {
+            myset.add(x);
+        }
+
+        for (int y: B) {
+            if (myset.contains(delta + y)) {
+                return new int[2] {delta + y, y}
+            }
+        }
+
+        return new int[0];
+    }
+
+
+
     public int[] fairCandySwap(int[] A, int[] B) {
         if (A.length <= 0 || A == null || B.length <= 0 || B == null) return null;
         int [] result = new int[2];
