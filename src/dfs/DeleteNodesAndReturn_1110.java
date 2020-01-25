ffcee45493609c74fbbffc1cdaf58bed8920489b
Chandler Fang <chandlerfang@gmail.com>
2020-01-26 01:53:52 +1100

    DeleteNodesAndReturn_1110

--- src/dfs/DeleteNodesAndReturn_1110.java
+++ src/dfs/DeleteNodesAndReturn_1110.java
@@ -19,10 +19,43 @@ public class DeleteNodesAndReturn_1110 {
 
     private TreeNode helper(TreeNode node, boolean is_root) {
         if (node == null) return null;
+
         boolean deleted = to_delete_set.contains(node.val);
+
         if (is_root && !deleted) res.add(node);
+
         node.left = helper(node.left, deleted);
         node.right =  helper(node.right, deleted);
+
         return deleted ? null : node;
+    }
+
+    public List<TreeNode> delNodesII(TreeNode root, int[] to_delete) {
+        List<TreeNode> forest = new ArrayList<>();
+        if (root == null) return forest;
+        Set<Integer> set = new HashSet<>();
+        for(int i : to_delete) {
+            set.add(i);
+        }
+        deleteNodes(root, set, forest);
+        if (!set.contains(root.val)) {
+            forest.add(root);
+        }
+        return forest;
+    }
+
+    private TreeNode deleteNodes(TreeNode node, Set<Integer> set, List<TreeNode> forest) {
+        if (node == null) return null;
+
+        node.left = deleteNodes(node.left, set, forest);
+        node.right = deleteNodes(node.right, set, forest);
+
+        if (set.contains(node.val)) {
+            if (node.left != null) forest.add(node.left);
+            if (node.right != null) forest.add(node.right);
+            return null;
+        }
+
+        return node;
     }
 }
