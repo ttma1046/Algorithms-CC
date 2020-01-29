538fac96e9ba6fedc43de8c74a6ca2c033e2a459
Chandler Fang <chandlerfang@gmail.com>
2020-01-29 23:12:48 +1100

    FloodFill_733

--- src/dfs/FloodFill_733.java
+++ src/dfs/FloodFill_733.java
@@ -1,8 +1,32 @@
 package dfs;
 
 public class FloodFill_733 {
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
 
+    public int[][] floodFillIII(int[][] image, int sr, int sc, int newColor) {
+        if (image == null || image.length <=0 || image[0].length <=0) {
+            return new int[0][0];
+        }
+        int color = image[sr][sc];
+        if (color != newColor) myDFS(image, sr, sc, newColor, color);
+
+        return image;
+    }
+
+    private void myDFS(int[][] image, int sr, int sc, int newColor, int color) {
+        if (color == image[sr][sc]) {
+            image[sr][sc] = newColor;
+            if (sr - 1 >= 0) myDFS(image, sr - 1, sc, newColor, color);
+
+            if (sr + 1 < image.length) myDFS(image, sr + 1, sc, newColor, color);
+
+            if (sc - 1 >= 0) myDFS(image, sr, sc - 1, newColor, color);
+
+            if (sc + 1 < image[0].length) myDFS(image, sr, sc + 1, newColor, color);
+        }
+    }
+
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         if (image == null || image.length <=0 || image[0].length <=0) {
             return new int[0][0];
         }
@@ -14,23 +38,27 @@ public class FloodFill_733 {
     }
 
     private void paint(int[][] image, int sr, int sc, int newColor, boolean[][] visited) {
+
         visited[sr][sc] = true;
         if (sr - 1 >= 0 && !visited[sr - 1][sc] && image[sr][sc] == image[sr - 1][sc])   {
+            image[sr][sc] = newColor;
             paint(image, sr - 1, sc, newColor, visited);
         }
 
         if (sr + 1 < image.length && !visited[sr + 1][sc] && image[sr][sc] == image[sr + 1][sc])   {
+            image[sr][sc] = newColor;
             paint(image, sr + 1, sc, newColor, visited);
         }
 
         if (sc - 1 >= 0 && !visited[sr][sc - 1] && image[sr][sc] == image[sr][sc - 1])   {
+            image[sr][sc] = newColor;
             paint(image, sr, sc - 1, newColor, visited);
         }
 
         if (sc + 1 < image[0].length && !visited[sr][sc + 1] && image[sr][sc] == image[sr][sc + 1])   {
+            image[sr][sc] = newColor;
             paint(image, sr, sc + 1, newColor, visited);
         }
-        image[sr][sc] = newColor;
     }
 
     public int[][] floodFillII(int[][] image, int sr, int sc, int newColor) {
@@ -49,7 +77,7 @@ public class FloodFill_733 {
     }
 
     public static void main(String[] args) {
-        int[][] result = new FloodFill_733().floodFill(new int[][] {{ 1, 1, 1 }, { 1, 1, 0 }, { 1, 0, 1 }}, 1, 1, 2);
+        int[][] result = new FloodFill_733().floodFillIII(new int[][] {{ 1, 1, 1 }, { 1, 1, 0 }, { 1, 0, 1 }}, 1, 1, 2);
 
         for(int[] i: result) {
             for(int j: i) {
