3036daef1565ca656d67b6b515225b0f29eb89a6
Chandler <chandlerfang@gmail.com>
2020-01-29 23:14:49 +1100

    BinaryTreePaths_257

---
+++ src/bfs/BinaryTreePaths_257.java
@@ -0,0 +1,31 @@
+package dfs;
+
+class BinaryTreePaths_257 {
+	public List<String> binaryTreePaths(TreeNode root) {
+		List<String> result = new ArrayList<String>();
+
+        if (root != null) {
+
+            StringBuilder sb = new StringBuilder();
+		    myDfs(root, result, sb);
+        }
+        
+		return result;
+    }
+
+    private void myDfs(TreeNode current, List<String> result, StringBuilder currentString) {
+        if (current == null) return;
+        
+        currentString.append(Integer.toString(current.val));
+        
+        if (current.right == null && current.left == null) {
+    	    result.add(currentString.toString());
+            return;
+        }
+        
+        currentString.append("->");
+        
+    	myDfs(current.left, result, currentString);
+    	myDfs(current.right, result, currentString);
+    }
+}
