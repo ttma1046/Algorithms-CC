488e8e0bc88aa60a70fca15654595b9e23c509bb
Chandler Fang <chandlerfang@gmail.com>
2020-01-30 21:53:17 +1100

    Path_Sum_112

--- src/dfs/Path_Sum_112.java
+++ src/dfs/Path_Sum_112.java
@@ -22,6 +22,45 @@ return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 
 class Path_Sum_112 {
     public boolean hasPathSum(TreeNode root, int sum) {
-        return false;
+        if (root == null) {
+            return false;
+        }
+
+        return myDFS(root, sum, 0);
+    }
+
+    private boolean myDFS(TreeNode current, int sum, int currentSum) {
+        if (current == null)
+            return false;
+
+        currentSum += current.val;
+
+        if (current.left == null && current.right == null) {
+            return currentSum == sum;
+        }
+
+        return myDFS(current.left, sum, currentSum) || myDFS(current.right, sum, currentSum);
+    }
+
+    public static void main(String[] args) {
+        TreeNode eleven = new TreeNode(11);
+        eleven.left = new TreeNode(7);
+        eleven.right = new TreeNode(2);
+
+        TreeNode four = new TreeNode(4);
+        four.left = eleven;
+
+        TreeNode anotherFour = new TreeNode(4);
+        anotherFour.right = new TreeNode(1);
+
+        TreeNode eight = new TreeNode(8);
+        eight.left = new TreeNode(13);
+        eight.right = anotherFour;
+
+        TreeNode root = new TreeNode(5);
+        root.left = four;
+        root.right = eight;
+
+        System.out.println(new Path_Sum_112().hasPathSum(root, 22));
     }
 }
