d6a58ca813253d630c7d05bd45511dc4ea767946
Chandler <chandlerfang@gmail.com>
2020-02-22 20:39:22 +1100

    fix.

--- src/resursion/Powersets.java
+++ src/resursion/Powersets.java
@@ -1,14 +1,13 @@
 package resursion;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 class Powersets {
     public static void main(String[] args) {
-        System.out.println(new Powersets().subsetsWithDupIte(new int[]{1, 2, 3}));
+        System.out.println(new Powersets().subsetsWithDupIte(new int[] { 1, 2, 3 }));
 
-        System.out.println(new Powersets().subsetsWithDupRec(new int[]{1, 2, 3}));
+        System.out.println(new Powersets().subsetsWithDupRec(new int[] { 1, 2, 3 }));
     }
 
     public List<List<Integer>> subsetsWithDupIte(int[] nums) {
@@ -18,8 +17,7 @@ class Powersets {
         List<List<Integer>> result = new ArrayList();
         result.add(new ArrayList<Integer>());
 
-
-        for(int num: nums) {
+        for (int num : nums) {
             int length = result.size();
             for (int i = 0; i < length; i++) {
                 List<Integer> subset = new ArrayList<Integer>(result.get(i));
@@ -73,7 +71,7 @@ class Powersets {
 
             ArrayList<ArrayList<Integer>> moreSubsets = new ArrayList<ArrayList<Integer>>();
 
-            for (ArrayList<Integer> subset: allSubsets) {
+            for (ArrayList<Integer> subset : allSubsets) {
 
                 ArrayList<Integer> newSubset = new ArrayList<Integer>(subset); //
                 newSubset.add(item);
@@ -85,4 +83,33 @@ class Powersets {
 
         return allSubsets;
     }
+
+    ArrayList<ArrayList<Integer>> getSubsets2(int x, ArrayList<Integer> set) {
+        ArrayList<ArrayList<Integer>> allsubsets = new ArrayList<ArrayList<Integer>>();
+
+        int max = 1 << set.size(); /* Compute 2^n */
+
+        for (int k = 0; k < max; k++) {
+            ArrayList<Integer> subset = convertIntToSet(k, set);
+
+            allsubsets.add(subset);
+        }
+
+        return allsubsets;
+    }
+
+    ArrayList<Integer> convertIntToSet(int x, ArrayList<Integer> set) {
+        ArrayList<Integer> subset = new ArrayList<Integer>();
+
+        int index = 0;
+        for (int k = x; k > 0; k >>= 1) {
+            if ((k & 1) == 1) {
+                subset.add(set.get(index));
+            }
+            index++;
+        }
+
+        return subset;
+    }
+
 }
