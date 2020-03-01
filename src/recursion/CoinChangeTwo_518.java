22eb61f958c19fe7adf47c8e74de8276c1d48f07
Chandler Fang <chandlerfang@gmail.com>
2020-03-02 00:45:09 +1100

    CoinChangeTwo_518

--- src/recursion/CoinChangeTwo_518.java
+++ src/recursion/CoinChangeTwo_518.java
@@ -61,15 +61,15 @@ public class CoinChangeTwo_518 {
     }
 
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount + 1];
-        dp[0] = 1;
+        int[] approaches = new int[amount + 1];
+        approaches[0] = 1;
 
         for (int coin : coins) {
           for (int x = coin; x < amount + 1; ++x) {
-            dp[x] += dp[x - coin];
+            approaches[x] += approaches[x - coin];
           }
         }
-        return dp[amount];
+        return approaches[amount];
     }
 
     public static void main(String[] args) {
