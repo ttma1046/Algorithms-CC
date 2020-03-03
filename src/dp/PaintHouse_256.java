96a77501def44275f033523495cacf7f9017e5b9
Chandler Fang <chandlerfang@gmail.com>
2020-03-04 01:47:04 +1100

    PaintHouse_256

--- src/dp/PaintHouse_256.java
+++ src/dp/PaintHouse_256.java
@@ -1,7 +1,40 @@
 package dp;
 
 class PaintHouse_256 {
-    public int minCoast(int[][] costs) {
-        return 0;
+    public int minCost(int[][] costs) {
+        int length = costs.length;
+
+        if (costs == null || costs.length <= 0) {
+            return 0;
+        }
+
+
+        ArrayList<int[]> costsArray = new ArrayList<int[]>();
+
+        for (int[] arr: costs) {
+            costsArray.add(arr);
+        }
+
+        return minCost(costsArray, costs, length);
+    }
+
+    public int minCostRec(ArrayList<int[]> costsArray, int[][] costs, int length) {
+        if (length == 1) {
+            return minCost(costs[0]);
+        }
+
+        return minCostRec(costsArray.cut(length - 1), costs, length - 1) + minCost(costs[length - 1]);
+    }
+
+    private static int minCost(int[] cost, int previousIndex) {
+        // return cost[0] > cost[1] ? (cost[1] > cost[2] ? cost[2] : cost[1]) : cost[0];
+
+        if (previousIndex == 0) {
+            return cost[1] > cost[2] ? cost[2] : cost[1];
+        } else if (previousIndex == 1) {
+            return cost[0] > cost[2] ? cost[2] : cost[0];
+        } else {
+            return cost[0] > cost[1] ? cost[1] : cost[0];
+        }
     }
 }
