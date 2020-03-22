9c55309a0323a45a6b84260b04119adad89d1378
Chandler <chandlerfang@gmail.com>
2020-03-22 12:43:47 +1100

    Permutations_with_dups:

--- src/recursion/Permutations_with_Dups.java
+++ src/recursion/Permutations_with_Dups.java
@@ -1,9 +1,6 @@
 package recursion;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
 /*
 Given a collection of numbers that might contain duplicates, return all possible unique permutations.
@@ -58,6 +55,29 @@ class Permutations_with_Dups {
             }
         }
     }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        backTracking(res, new ArrayList<>(), nums, new boolean[nums.length]);
+        return res;
+    }
+
+    public void backTracking(List<List<Integer>> resultList, List<Integer> current, int[] nums, boolean[] used) {
+        if( current.size() == nums.length )
+            resultList.add(new ArrayList<Integer>(current));
+        else{
+            for(int i = 0; i < nums.length; i++) {
+                if( used[i] || ( i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) ) // remove duplicate
+                    continue;
+                current.add(nums[i]);
+                used[i] = true;
+                backTracking(resultList, current, nums, used);
+                used[i] = false;
+                current.remove(current.size() - 1);
+            }
+        }
+    }
     
     public static void main(String[] args) {
         List<List<Integer>> result = new Permutations_with_Dups().permute(new int [] {1, 1, 3});
@@ -67,5 +87,46 @@ class Permutations_with_Dups {
                 System.out.println(num);
             }
         }
+    }
+
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> permuteUniqueII(int[] nums) {
+        if (nums.length != 0) {
+            helper(nums, 0);
+        }
+
+        return res;
+    }
+
+    private void helper(int[] nums, int index) {
+        if (index == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for (int n : nums) list.add(n);
+            res.add(list);
+
+            return;
+        }
+
+        for (int i=index; i<nums.length; i++) {
+            if (check(nums, index, i)) continue;
+
+            swap(nums, i, index);
+            helper(nums, index+1);
+            swap(nums, i, index);
+        }
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
+
+    private boolean check(int[] nums, int s, int e) {
+        for (int i=s; i<e; i++) {
+            if (nums[i] == nums[e]) return true;
+        }
+
+        return false;
     }
 }
