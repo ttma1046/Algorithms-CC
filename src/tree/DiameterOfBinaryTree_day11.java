4d1d872f38f6fc76a30cba4000b3e7dbf1762018
Chandler <chandlerfang@gmail.com>
2020-04-11 21:48:58 +1000

    passing by reference

--- src/tree/DiameterOfBinaryTree_day11.java
+++ src/tree/DiameterOfBinaryTree_day11.java
@@ -37,3 +37,37 @@ class DiameterOfBinaryTree_day11 {
         return (1 + Math.max(height(node.left), height(node.right)));
     }
 }
+
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return 0;
+        }
+        int [] max = new int[1];
+        travsel(root, max);
+        return max[0];
+    }
+
+    private int travsel(TreeNode node, int[] max) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftLength = travsel(node.left, max);
+
+        int rightLength = travsel(node.right, max);
+
+        max[0] = Math.max(max[0], leftLength + rightLength);
+
+        return Math.max(leftLength, rightLength) + 1;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
