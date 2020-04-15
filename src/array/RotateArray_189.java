e9ac71330b9aa35ff9c2570489c823bdfa3a0526
Chandler Fang <chandlerfang@gmail.com>
2020-04-15 21:33:50 +1000

    RotateArray_189 PerformStringShifts_Day14 RotateString_796

---
+++ src/array/RotateArray_189.java
@@ -0,0 +1,17 @@
+class RotateArray_189 {
+  public void rotate(int[] nums, int k) {
+    k %= nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+  }
+  public void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+      nums[start] = nums[start] + nums[end];
+      nums[end] = nums[start] - nums[end];
+      nums[start] = nums[start] - nums[end];
+      start++;
+      end--;
+    }
+  }
+}
