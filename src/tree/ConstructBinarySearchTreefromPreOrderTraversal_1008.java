1dd8affa2ea62d200fae177f3b09815c0b64a776
Chandler <chandlerfang@gmail.com>
2020-05-12 23:40:20 +1000

    ConstructBinarySearchTreefromPreOrderTraversal_1008

---
+++ src/tree/ConstructBinarySearchTreefromPreOrderTraversal_1008.java
@@ -0,0 +1,105 @@
+package tree;
+
+import java.util.Stack;
+
+
+public class ConstructBinarySearchTreefromPreOrderTraversal_1008 {
+    /*
+    public TreeNode bstFromPreorder(int[] preorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+
+        int n = preorder.length;
+        TreeNode node = new TreeNode(preorder[0]);
+        Stack<TreeNode> myStack = new Stack<TreeNode>();
+        myStack.push(node);
+
+        for(int i = 1; i < n; i++) {
+            TreeNode temp = myStack.peek();
+            TreeNode child = new TreeNode(preorder[i]);
+
+            while(!myStack.isEmpty() && myStack.peek().val < child.val) {
+                temp = myStack.pop();
+            }
+
+            if (child.val > temp.val) {
+                temp.right = child;
+            } else {
+                temp.left = child;
+            }
+
+            myStack.push(child);
+        }
+
+        return node;
+    }
+    */
+    /*
+    int idx = 0;
+    int[] preorder;
+    int n;
+
+    public TreeNode helper(int lower, int upper) {
+        // if all elements from preorder are used
+        // then the tree is constructed
+        if (idx == n) return null;
+
+        int val = preorder[idx];
+        // if the current element
+        // couldn't be placed here to meet BST requirements
+        if (val < lower || val > upper) return null;
+
+        // place the current element
+        // and recursively construct subtrees
+        idx++;
+        TreeNode root = new TreeNode(val);
+        root.left = helper(lower, val);
+        root.right = helper(val, upper);
+        return root;
+    }
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        this.preorder = preorder;
+        n = preorder.length;
+        return helper(Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    */
+    int index = 0;
+    private TreeNode helper(int lower, int upper, int[] array) {
+    	if (index == array.length) {
+    		return null;
+    	}
+
+    	int val = array[index];
+    	if (val < lower || val > upper) return null;
+    	index++;
+    	TreeNode node = new TreeNode(val);
+        node.left = helper(lower, val, array);
+        node.right = helper(val, upper, array);
+        return node;
+    }
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+    	if (preorder == null || preorder.length <= 0) {
+    		return null;
+    	}
+
+    	return helper(Integer.MIN_VALUE, Integer.MAX_VALUE, preorder);
+   	}
+
+    public static void main(String[] args) {
+        TreeNode result = new ConstructBinarySearchTreefromPreOrderTraversal_1008().bstFromPreorder(new int[] { 8, 5, 1, 7, 10, 12 });
+
+        printPreOrder(result);
+    }
+
+    private static void printPreOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.val);
+        printPreOrder(node.left);
+        printPreOrder(node.right);
+    }
+}
