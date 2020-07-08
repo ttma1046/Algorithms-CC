9a8d6a113074afbe100ea4e25f1272fbfcc639fc
Chandler <chandlerfang@gmail.com>
2020-07-08 23:56:26 +1000

    Task_Scheduler_621

--- src/greedy/Jump_Game_II_45.java
+++ src/greedy/Jump_Game_II_45.java
@@ -1,6 +1,5 @@
 package greedy;
 
-/*
 Given an array of non-negative integers, you are initially positioned at the first index of the array.
 
 Each element in the array represents your maximum jump length at that position.
@@ -16,10 +15,38 @@ Explanation: The minimum number of jumps to reach the last index is 2.
 Note:
 
 You can assume that you can always reach the last index.
-*/
 
 class Jump_Game_II {
     public int jump(int[] nums) {
+        if (nums == null || nums.length <= 0) {
+            return -1;
+        }
+
+        int lastPos = nums.length - 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+
+        return lastPos == 0;
+    }
+
+
+    public int jump(int[] nums) {
+        if (nums == null || nums.length <= 0) {
+            return -1;
+        }
+
+        int minSteps = 0;
+
+        for(int i = 0; i < nums.length; i++) {
             
+            
+            currentSteps++;
+            minSteps = Math.min(minSteps, currentSteps);
+        }
+        return minSteps
     }
 }
