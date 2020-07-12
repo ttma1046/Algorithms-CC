dc901ed47d3b54ba30a768cadbf8261070e15bc5
Chandler Fang <chandlerfang@gmail.com>
2020-07-12 17:59:25 +1000

    Reorganized_String_767

--- src/greedy/Reorganized_String_767.java
+++ src/greedy/Reorganized_String_767.java
@@ -18,49 +18,94 @@ S will consist of lowercase letters and have length in range [1, 500].
 package greedy;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.HashMap;
+import java.util.Map;
+
 
 class Reorganized_String_767 {
     /*
      * Approach #1: Sort by Count [Accepted] Intuition
-     * 
+     *
      * If we should make no two 'a's adjacent, it is natural to write "aXaXaXa..."
      * where "X" is some letter. For now, let's assume that the task is possible
      * (ie. the answer is not "".)
-     * 
+     *
      * Let's sort the string S, so all of the same kind of letter occur in
      * continuous blocks. Then when writing in the following interleaving pattern,
      * like S[3], S[0], S[4], S[1], S[5], S[2], adjacent letters never touch. (The
      * specific interleaving pattern is that we start writing at index 1 and step by
      * 2; then start from index 0 and step by 2.)
-     * 
+     *
      * The exception to this rule is if N is odd, and then when interleaving like
      * S[2], S[0], S[3], S[1], S[4], we might fail incorrectly if there is a block
      * of the same 3 letters starting at S[0] or S[1]. To prevent failing
      * erroneously in this case, we need to make sure that the most common letters
      * all occur at the end.
-     * 
+     *
      * Finally, it is easy to see that if N is the length of the string, and the
      * count of some letter is greater than (N + 1) / 2, the task is impossible.
-     * 
+     *
      * Algorithm
-     * 
+     *
      * Find the count of each character, and use it to sort the string by count.
-     * 
+     *
      * If at some point the number of occurrences of some character is greater than
      * (N + 1) / 2, the task is impossible.
-     * 
+     *
      * Otherwise, interleave the characters in the order described above.
-     * 
-     * 
+     *
+     *
      * Complexity Analysis
-     * 
+     *
      * Time Complexity: O(A(N + logA)), where N is the length of S, and A is the
      * size of the alphabet. In Java, our implementation is O(N + AlogA). If A is
      * fixed, this complexity is O(N).
-     * 
+     *
      * Space Complexity: O(N). In Java, our implementation is O(N + A).
      */
-    public String reorganizeString(String S) {
+
+    public String reorganizeStringMy(String s) {
+        if (s.length() <= 0 || s == null) {
+            return "";
+        }
+
+        int length = s.length();
+        int[] counts = new int[26];
+
+        for (char c : s.toCharArray()) {
+            counts[c - 'a'] += 100;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            counts[i] += i;
+        }
+
+        Arrays.sort(counts);
+
+        char[] result = new char[length];
+        int index = 1;
+        for (int count : counts) {
+            if (count > 25) {
+                int frequency = count / 100;
+
+                if (frequency > (length + 1) / 2) { return ""; }
+
+                char cr =  (char) ('a' + (count % 100));
+
+                for (int i = 0; i < frequency; i++) {
+                    if (index >= length) index = 0;
+                    result[index] = cr;
+
+                    index += 2;
+                }
+            }
+        }
+
+        return String.valueOf(result);
+    }
+
+    public String reorganizeStringI(String S) {
         int length = S.length();
         int[] counts = new int[26];
         for (char c : S.toCharArray())
@@ -97,7 +142,7 @@ class Reorganized_String_767 {
 
     /*
      * No Sort O(N):
-     * 
+     *
      * 1. count letter appearance and store in hash[i] 2. find the letter with
      * largest occurence. 3. put the letter into even index numbe (0, 2, 4 ...) char
      * array 4. put the rest into the array
@@ -139,63 +184,63 @@ class Reorganized_String_767 {
 
     /*
      * Approach #2: Greedy with Heap [Accepted] Intuition
-     * 
+     *
      * One consequence of the reasoning in Approach #1,
-     * 
+     *
      * is that a greedy approach that tries to write the most common letter (that
      * isn't the same as the previous letter written) will work.
-     * 
+     *
      * The reason is that the task is only impossible if the frequency of a letter
      * exceeds (N+1) / 2.
-     * 
+     *
      * Writing the most common letter followed by the second most common letter
      * keeps this invariant.
-     * 
+     *
      * A heap is a natural structure to repeatedly return the current top 2 letters
      * with the largest remaining counts.
-     * 
+     *
      * Approach
-     * 
+     *
      * We store a heap of (count, letter). [In Python, our implementation stores
      * negative counts.]
-     * 
+     *
      * We pop the top two elements from the heap (representing different letters
      * with positive remaining count),
-     * 
+     *
      * and then write the most frequent one that isn't the same as the most recent
      * one written.
-     * 
+     *
      * After, we push the correct counts back onto the heap.
-     * 
+     *
      * Actually, we don't even need to keep track of the most recent one written.
-     * 
+     *
      * If it is possible to organize the string, the letter written second can never
      * be written first in the very next writing.
-     * 
+     *
      * At the end, we might have one element still on the heap, which must have a
      * count of one.
-     * 
+     *
      * If we do, we'll add that to the answer too.
-     * 
+     *
      * Proof of Invariant
-     * 
+     *
      * The invariant mentioned in the [Intuition] section seems true when playing
      * with it, but here is a proof. Let Ci be the count of each letter yet to be
      * written, and N be the number of letters left to write. We want to show this
      * procedure maintains the invariant 2 ∗ max(Ci) <= N + 1 ​ i Say C'_i are the
      * counts after one writing step.
-     * 
+     *
      * This completes the proof of this invariant.
-     * 
+     *
      * if max(Ci) > 3rdmax(Ci), then (C'_i) <= max(Ci) - 1, so 2max(C'i) <= 2max(Ci)
      * - 2 <= N - 1 as desired. if M = max(Ci) = 3rdmax(Ci), then 3M <= N, Also
      * ,because M >= 1, N + 3. Then, 2M <= 2N/3 <= N -1 as desired.
-     * 
+     *
      * Complexity Analysis
-     * 
+     *
      * Time Complexity: O(NlogA)), where N is the length of S, and A is the size of
      * the alphabet. If A is fixed, this complexity is O(N).
-     * 
+     *
      * Space Complexity: O(A). If A is fixed, this complexity is O(1).
      */
     public String reorganizeStringII(String S) {
@@ -208,7 +253,7 @@ class Reorganized_String_767 {
         }
 
         PriorityQueue<MultiChar> pq = new PriorityQueue<MultiChar>(
-                (a, b) -> a.count == b.count ? a.letter - b.letter : b.count - a.count);
+            (a, b) -> a.count == b.count ? a.letter - b.letter : b.count - a.count);
 
         for (int i = 0; i < 26; ++i) {
             if (count[i] > 0) {
@@ -291,7 +336,7 @@ class Reorganized_String_767 {
      * Space O(N + 26): result + hash[]
      */
     public static void main(String[] args) {
-        System.out.println(new Reorganized_String_767().reorganizeString("ccaaabb"));
+        System.out.println(new Reorganized_String_767().reorganizeStringMy("ccaaabb"));
     }
 }
 
