a4aaf170fdd30441bae51fad449b5344b72db7e3
Chandler <chandlerfang@gmail.com>
2020-08-06 00:18:49 +1000

    MediumofTwoSortedArray_4

--- src/binarysearch/MediumofTwoSortedArray_4.java
+++ src/binarysearch/MediumofTwoSortedArray_4.java
@@ -21,12 +21,8 @@ public class MediumofTwoSortedArray_4 {
 
     The median is (2 + 3)/2 = 2.5
     */
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
-
+    public double findMedianSortedArraysII(int[] nums1, int[] nums2) {
         // merge two sorted array
-        // binaryseach the middle
-
         int length = nums1.length + nums2.length;
 
         int[] mergedArray = new int[length];
@@ -52,32 +48,45 @@ public class MediumofTwoSortedArray_4 {
         }
     }
 
-    public double findMedianSortedArrays(int[] A, int[] B) {
-        int m = A.length;
-        int n = B.length;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
         if (m > n) { // to ensure m<=n
-            int[] temp = A; A = B; B = temp;
+            int[] temp = nums1; nums1 = nums2; nums2 = temp;
             int tmp = m; m = n; n = tmp;
         }
+
         int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
         while (iMin <= iMax) {
             int i = (iMin + iMax) / 2;
             int j = halfLen - i;
-            if (i < iMax && B[j-1] > A[i]){
+            if (i < iMax && nums2[j - 1] > nums1[i]) {
                 iMin = i + 1; // i is too small
-            } else if (i > iMin && A[i-1] > B[j]) {
+            } else if (i > iMin && nums1[i - 1] > nums2[j]) {
                 iMax = i - 1; // i is too big
             } else { // i is perfect
                 int maxLeft = 0;
-                if (i == 0) { maxLeft = B[j-1]; }
-                else if (j == 0) { maxLeft = A[i-1]; }
-                else { maxLeft = Math.max(A[i-1], B[j-1]); }
-                if ( (m + n) % 2 == 1 ) { return maxLeft; }
+                
+                if (i == 0) { 
+                    maxLeft = nums2[j - 1]; 
+                } else if (j == 0) { 
+                    maxLeft = nums1[i - 1]; 
+                } else { 
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]); 
+                }
+
+                if ((m + n) % 2 == 1) { 
+                    return maxLeft; 
+                }
 
                 int minRight = 0;
-                if (i == m) { minRight = B[j]; }
-                else if (j == n) { minRight = A[i]; }
-                else { minRight = Math.min(B[j], A[i]); }
+                if (i == m) { 
+                    minRight = nums2[j]; 
+                } else if (j == n) { 
+                    minRight = nums1[i]; 
+                } else { 
+                    minRight = Math.min(nums2[j], nums1[i]); 
+                }
 
                 return (maxLeft + minRight) / 2.0;
             }
@@ -89,44 +98,5 @@ public class MediumofTwoSortedArray_4 {
     public static void main(String[] args) {
         System.out.println(new MediumofTwoSortedArray_4().findMedianSortedArrays(new int[] {1, 3}, new int[] {2}));
         System.out.println(new MediumofTwoSortedArray_4().findMedianSortedArrays(new int[] {1, 2}, new int[] {3, 4}));
-    }
-
-    // findKthElement
-    public int BinarySearch() {
-
-
-        // first time:
-        // k = 5
-        // k/2 - 1 = 1
-
-        // 1, 1, 1, 7, 9
-        // 2, 4, 6, 8
-
-        // cos 1 < 4
-        // 1, 7, 9
-        // 2, 4, 6, 8
-
-        // second time:
-        // k = 3
-        // k/2 - 1 = 0
-
-        // cos 1 < 2
-        // 7, 9
-        // 2, 4, 6, 8
-
-        // third time:
-        // k = 2
-        // k/2 - 1 = 0
-
-        // cos 2 < 7
-        // 7, 9
-        // 4, 6, 8
-
-        // fourth time:
-        // k = 1
-        // cos 4 < 7
-        // so result = 4.
-
-        return 0;
     }
 }
