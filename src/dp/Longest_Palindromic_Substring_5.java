524e76088bf13bbdbe74f85e7f2f0e334e698838
Chandler Fang <chen.fang@transport.nsw.gov.au>
2020-08-10 23:22:11 +1000

    Longest_Palindromic_Substring_5

--- src/dp/Longest_Palindromic_Substring_5.java
+++ src/dp/Longest_Palindromic_Substring_5.java
@@ -1,17 +1,74 @@
 package dp;
+/*
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+Example 2:
+
+Input: "cbbd"
+Output: "bb"
+*/
+
+/*
+How can we reuse a previously computed palindrome to compute a larger palindrome?
+*/
+
+/*
+If “aba” is a palindrome, is “xabax” and palindrome? Similarly is “xabay” a palindrome?
+*/
+
+/*
+Complexity based hint:
+If we use brute-force and check whether for every start and end position a substring is a palindrome
+we have O(n^2) start - end pairs and O(n) palindromic checks.
+Can we reduce the time for palindromic checks to O(1) by reusing some previous computation.
+*/
+
 class Longest_Palindromic_Substring_5 {
 	public String longestPalindrome(String s) {
-		if (s == null || s.length() < 1) return "";
-		int start = 0, end = 0;
+		int n = s.length();
+		String res = "";
+
+		boolean[][] dp = new boolean[n][n];
+
+		for (int i = n - 1; i >= 0; i--) {
+			for (int j = i; j < n; j++) {
+				dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+
+				if (dp[i][j] && (res == null || j - i + 1 > res.length())) {
+					res = s.substring(i, j + 1);
+				}
+			}
+		}
+
+		return res;
+	}
+
+
+	public String longestPalindrome(String s) {
+		if (s == null || s.length() < 1) {
+			return "";
+		}
+
+		int start = 0;
+		int end = 0;
 		for (int i = 0; i < s.length(); i++) {
 			int len1 = expandAroundCenter(s, i, i);
+
 			int len2 = expandAroundCenter(s, i, i + 1);
+
 			int len = Math.max(len1, len2);
+
 			if (len > end - start) {
 				start = i - (len - 1) / 2;
 				end = i + len / 2;
 			}
 		}
+
 		return s.substring(start, end + 1);
 	}
 
