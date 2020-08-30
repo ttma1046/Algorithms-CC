845c63e1fc70fea24853e5cb4102563b7e80b0c7
Chandler <chandlerfang@gmail.com>
2020-08-31 00:40:17 +1000

    Three_Sum_15

---
+++ src/twopointers/Three_Sum_15.java
@@ -0,0 +1,41 @@
+package twopointers;
+
+/*
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+
+class Three_Sum_15 {
+	public List<List<Integer>> threeSum(int[] nums) {
+		Arrays.sort(nums);
+		List<List<Integer>> res = new LinkedList<>();
+		for (int i = 0; i < nums.length - 2; i++) {
+			if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+				int lo = i + 1, hi = nums.length - 1, sum = 0 - nums[i];
+				while (lo < hi) {
+					if (nums[lo] + nums[hi] == sum) {
+						res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+						while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+						while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
+						lo++; hi--;
+					} else if (nums[lo] + nums[hi] < sum) lo++;
+					else hi--;
+				}
+			}
+		}
+		return res;
+	}
+}
