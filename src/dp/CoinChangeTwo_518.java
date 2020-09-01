8acedb344934414f9d7c42acfb4166fabeb9b306
Chandler Fang <chandlerfang@gmail.com>
2020-09-02 01:33:59 +1000

    CoinChangeTwo_518

--- src/dp/CoinChangeTwo_518.java
+++ src/dp/CoinChangeTwo_518.java
@@ -1,5 +1,5 @@
 package dp;
-import java.util.HashMap;
+import java.util.Arrays;
 /*
     You are given coins of different denominations and a total amount of money.
     Write a function to compute the number of combinations that make up that amount.
@@ -37,13 +37,13 @@ import java.util.HashMap;
     the answer is guaranteed to fit into signed 32-bit integer
 */
 public class CoinChangeTwo_518 {
-    public int changeBest(int[] coins, int amount) {
+    public int changeIII(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
 
         dp[0] = 1;
 
-        for (int coin: coins) {
-            for (int i = coin; i <= amount; ++i) {
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
                 dp[i] += dp[i - coin];
             }
         }
@@ -51,162 +51,128 @@ public class CoinChangeTwo_518 {
         return dp[amount];
     }
 
+
     public static void main(String[] args) {
-        System.out.println(new CoinChangeTwo_518().changeBest(new int[] { 1, 2, 5 }, 5));
+        System.out.println(new CoinChangeTwo_518().change(new int[] { 1, 2, 5 }, 5));
 
-        System.out.println(new CoinChangeTwo_518().changeBest(new int[] { 16, 30, 9, 17, 40, 13, 42, 5, 25, 49, 7, 23, 1, 44, 4, 11, 33, 12, 27,
+        /*
+        System.out.println(new CoinChangeTwo_518().change(new int[] { 16, 30, 9, 17, 40, 13, 42, 5, 25, 49, 7, 23, 1, 44, 4, 11, 33, 12, 27,
                            2, 38, 24, 28, 32, 14, 50
-                                                                           }, 245));
+                                                                        }, 245));
 
-        long result = new CoinChangeTwo_518().changeBest(new int [] { 2, 5, 10 }, 11);
+        long result = new CoinChangeTwo_518().change(new int [] { 2, 5, 10 }, 11);
         System.out.println(result);
 
-        result = new CoinChangeTwo_518().changeBest(new int [] { 10 }, 10);
+        result = new CoinChangeTwo_518().change(new int [] { 10 }, 10);
         System.out.println(result);
+        */
+    }
+
+    public int changeII(int amount, int[] coins) {
+        return makeChange(coins, amount, 0, new int[amount + 1][coins.length]);
+    }
+
+    public int makeChange(int[] coins, int remaining, int index, int[][] memo) {
+        if (remaining == 0) {
+            return 1;
+        }
+
+        if (index >= coins.length) {
+            return 0;
+        }
+
+        if (memo[remaining][index] > 0) {
+            return memo[remaining][index];
+        }
+
+        int amountWithCoin = 0;
+        int ways = 0;
+        while (amountWithCoin <= remaining) {
+            ways += makeChange(coins, remaining - amountWithCoin, index + 1, memo);
+            amountWithCoin += coins[index];
+        }
+        memo[remaining][index] = ways;
+        return memo[remaining][index];
     }
 
     public int change(int[] coins, int amount) {
-        int[][] dp = new int[coins.length + 1][amount + 1];
-        dp[0][0] = 1;
+        // order coins in order to prune recursion
+        Arrays.sort(coins);
 
-        for (int i = 1; i <= coins.length; i++) {
-            dp[i][0] = 1;
-            for (int j = 1; j <= amount; j++) {
-                dp[i][j] = dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0);
+        // init memorization to -1 (unvisited)
+        int[][] map = new int[amount + 1][coins.length];
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                map[i][j] = -1;
             }
         }
-        return dp[coins.length][amount];
+
+        // DFS
+        return Count(coins, amount, 0, map, 0);
     }
 
-    public int[] Change(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        dp[0] = 1;
-
-        for (int x = 0; x < amount + 1; ++x) {
-            for (int coin : coins) {
-                if (x - coin >= 0) dp[x] += dp[x - coin];
+    public int Count(int[] coins, int remaining, int index, int[][] map, int level) {
+        if (remaining == 0) {
+            for (int a = level; a >= 0; a--) {
+                System.out.print("   ");
             }
+            System.out.println("Got it");
+            return 1;
         }
-        return dp;
-    }
+        if (index >= coins.length) return 0;
+        if (map[remaining][index] != -1) {
+            for (int p = level; p >= 0; p--) {
+                System.out.print("   ");
+            }
+            System.out.println("Return Cache:" + remaining + ":" + coins[index] + ":" + map[remaining][index]);
+            return map[remaining][index];
+        }
 
-    // O(nd) time | O(n) space
-    public static int numberOfWaysToMakeChange(int[] denoms, int n) {
-        int [] ways = new int[n + 1];
-        ways[0] = 1;
-        for(int denom: denoms) {
-            for(int targetAmount = 1; targetAmount < ways.length; targetAmount++) {
-                if (denom <= targetAmount) {
-                    ways[targetAmount] += ways[targetAmount - denom];
+        int cnt = 0;
+        for (int i = index; i < coins.length; i++) {
+            for (int b = level; b >= 0; b--) {
+                System.out.print("   ");
+            }
+            System.out.println("Pick Coin " + coins[i]);
+            if (coins[i] > remaining) {
+                for (int c = level; c >= 0; c--) {
+                    System.out.print("   ");
+                }
+                System.out.println("End Coin " + coins[i]);
+                break;
+            }
+
+            // using this coin as many times as possible before going to next coin
+            int times = 1;
+            while (times * coins[i] <= remaining) {
+                for (int d = level; d >= 0; d--) {
+                    System.out.print("   ");
+                }
+                System.out.println("While Coin " + coins[i] + ":Get " + times + " Coins: Total " + times * coins[i]);
+                for (int e = level; e >= 0; e--) {
+                    System.out.print("   ");
+                }
+
+                System.out.println("Remaining :" + (remaining - times * coins[i]));
+                cnt += Count(coins, remaining - times * coins[i], i + 1, map, level + 1);
+                times++;
+                if (times * coins[i] > remaining) {
+                    for (int f = level; f >= 0; f--) {
+                        System.out.print("   ");
+                    }
+                    System.out.println("End while");
                 }
             }
         }
+        for (int q = level; q >= 0; q--) {
+            System.out.print("   ");
+        }
+        System.out.println("Save Cache:" + remaining + ":" + coins[index] + ":" + cnt);
 
 
-        return ways[n];
+        // memorize
+        map[remaining][index] = cnt;
+        return cnt;
     }
-
-    public static long makeChangeLong(int[] coins, int money) {
-        return makeChangeLong(coins, money, 0, new HashMap<String, Long>());
-    }
-
-    public static long makeChangeLong(int[] coins, int money, int index, HashMap<String, Long> memo) {
-        if (money == 0) {
-            return 1;
-        }
-
-        if (index >= coins.length) {
-            return 0;
-        }
-
-        String key = money + "-" + index; // "29" + "1" || "2" + "91"
-
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        }
-
-        int amountWithCoin = 0;
-        long ways = 0;
-        while (amountWithCoin <= money) {
-            int remaining = money - amountWithCoin;
-            ways += makeChangeLong(coins, remaining, index + 1, memo);
-            amountWithCoin += coins[index];
-        }
-        memo.put(key, ways);
-        return ways;
-    }
-
-    public long makeChangeII(int[] coins, int money) {
-        return makeChangeII(coins, money, 0);
-    }
-
-    private long makeChangeII(int[] coins, int money, int index) {
-        if (money == 0) {
-            return 1;
-        }
-
-        if (index >= coins.length) {
-            return 0;
-        }
-
-        int amountWithCoin = 0;
-        int ways = 0;
-        while (amountWithCoin <= money) {
-            int remaining = money - amountWithCoin;
-            ways += makeChangeII(coins, remaining, index + 1);
-            amountWithCoin += coins[index];
-        }
-        return ways;
-    }
-
-    /*
-    This works, but it's not as optimal as it could be.
-    The issue is that we will be recursively calling makeChange several times for the same values of amount and index.
-    We can resolve this issue by storing the previously computed values.
-    We'll need to store a mapping from each pair (amount, index) to the precomputed result.
-    */
-    int makeChange(int n, int[] denoms) {
-        int[][] map = new int[n + 1][denoms.length];
-        // return makeChange(n, denoms, 0);
-        return makeChangeII(n, denoms, 0, map);
-    }
-
-    int makeChange(int amount, int[] denoms, int index) {
-        if (index >= denoms.length - 1) return 1; // last denom;
-        int denomAmount = denoms[index];
-        int ways = 0;
-
-        for (int i = 0; i * denomAmount <= amount; i++) {
-            int amountRemaining = amount - i * denomAmount;
-            ways += makeChange(amountRemaining, denoms, index + 1);
-        }
-
-        return ways;
-    }
-
-
-    int makeChangeII(int amount, int[] denoms, int index, int[][] map) {
-        if (map[amount][index] > 0) {
-            // retrieve value
-            return map[amount][index];
-        }
-
-        if (index >= denoms.length - 1) return 1; // one denom remaining;
-        int denomAmount = denoms[index];
-        int ways = 0;
-        for (int i = 0; i * denomAmount <= amount; i++) {
-            // got to next denom, assuming i coins of denomAmount
-            int amountRemaining = amount - i * denomAmount;
-            ways += makeChangeII(amountRemaining, denoms, index + 1, map);
-        }
-        map[amount][index] = ways;
-        return ways;
-    }
-    /*
-    Note that we've used a two-dimensional array of integers to store the previously computed values.
-    This is simpler, but takes up a little extra space.
-    Alternatively, we could use an actual hash table that maps from amount to a new hash table,
-    which then maps from denom to the precomputed value.
-    There are other alternative data structures as well.
-    */
 }
