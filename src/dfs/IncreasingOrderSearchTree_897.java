0cf77cfb16cbf390c0020de53e5eee175ebdf050
Chandler <chandlerfang@gmail.com>
2020-09-14 00:20:47 +1000

    IncreasingOrderSearchTree_897

--- src/dfs/IncreasingOrderSearchTree_897.java
+++ src/dfs/IncreasingOrderSearchTree_897.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
-Given a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only 1 right child.
+Given a binary search tree, rearrange 
+
+the tree in in-order so that the leftmost node in the tree is now the root of the tree,
+
+and every node has no left child and only 1 right child.
 
 Example 1:
 Input: [5,3,6,2,4,null,8,1,null,null,null,7,9]
