0f534af26cc73ec8868fbe11cafa010c5a153527
Chandler Fang <chandlerfang@gmail.com>
2020-10-04 23:16:42 +1100

    dfs/RangeSumofBST_938

--- src/dfs/RangeSumofBST_938.java
+++ src/dfs/RangeSumofBST_938.java
@@ -19,24 +19,6 @@ package dfs;
 */
 
 class RangeSumofBST_938 {
-    int ans;
-    public int rangeSumBST(TreeNode root, int L, int R) {
-        ans = 0;
-        dfs(root, L, R);
-        return ans;
-    }
-
-    public void dfs(TreeNode node, int L, int R) {
-        if (node != null) {
-            if (L <= node.val && node.val <= R)
-                ans += node.val;
-            if (L < node.val)
-                dfs(node.left, L, R);
-            if (node.val < R)
-                dfs(node.right, L, R);
-        }
-    }
-
     public int rangeSumBST(TreeNode root, int L, int R) {
         if (root == null) return 0;
         return dfs(root, L, R);
@@ -71,22 +53,35 @@ class RangeSumofBST_938 {
 
             if (node == null) continue;
 
-            if (node != null) {
-                if (node.val >= L && node.val <= R) {
-                    result += node.val;
-                } 
-                if (node.val > L) {
-                    stack.push(node.right);
-                }
+            if (node.val >= L && node.val <= R) {
+                result += node.val;
+            }
+            if (node.val > L) {
+                stack.push(node.right);
+            }
 
-                if (node.val < R) {
-                    stack.push(node.left);
-                }
+            if (node.val < R) {
+                stack.push(node.left);
             }
         }
 
         return result;
     }
+
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null) return 0; // base case.
+        if (root.val < L) return rangeSumBST(root.right, L, R); // left branch excluded.
+        if (root.val > R) return rangeSumBST(root.left, L, R); // right branch excluded.
+        return root.val + rangeSumBST(root.right, L, R) + rangeSumBST(root.left, L, R); // count in both children.
+    }
+
+    /*
+    Complexity Analysis
+
+    Time Complexity: O(N)O(N), where NN is the number of nodes in the tree.
+
+    Space Complexity: O(H)O(H), where HH is the height of the tree.
+    */
 
     public static void main(String[] args) {
         final TreeNode five = new TreeNode(5);
