b3b51516cf9bc53ce9451f7804702fde887791c1
ttma1046 <chandlerfang@gmail.com>
2021-01-13 01:10:25 +1100

    Max_Consecutive_Ones_III_1004 Count_Number_of_Nice_Subarrays_1248

--- src/twopointers/Replace_the_Substring_for_Balanced_String_1234.java
+++ src/twopointers/Replace_the_Substring_for_Balanced_String_1234.java
@@ -1,3 +1,4 @@
+package twopointers;
 /*
 Intuition
 We want a minimum length of substring,
@@ -55,20 +56,22 @@ Space O(1)
 Java
 */
 
-public int balancedString(String s) {
-    int[] count = new int[128];
-    int n = s.length(), res = n, i = 0, k = n / 4;
-    for (int j = 0; j < n; ++j) {
-        ++count[s.charAt(j)];
-    }
-    for (int j = 0; j < n; ++j) {
-        --count[s.charAt(j)];
-        while (i < n && count['Q'] <= k && count['W'] <= k && count['E'] <= k && count['R'] <= k) {
-            res = Math.min(res, j - i + 1);
-            ++count[s.charAt(i++)];
+class Replace_the_Substring_for_Balanced_String_1234 {
+    public int balancedString(String s) {
+        int[] count = new int[128];
+        int n = s.length(), res = n, i = 0, k = n / 4;
+        for (int j = 0; j < n; ++j) {
+            ++count[s.charAt(j)];
         }
+        for (int j = 0; j < n; ++j) {
+            --count[s.charAt(j)];
+            while (i < n && count['Q'] <= k && count['W'] <= k && count['E'] <= k && count['R'] <= k) {
+                res = Math.min(res, j - i + 1);
+                ++count[s.charAt(i++)];
+            }
+        }
+        return res;
     }
-    return res;
 }
 
 
