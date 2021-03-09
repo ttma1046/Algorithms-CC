dfbf9e06c583acf2de888cbc20a2ed5c37f873fa
Chandler <chandlerfang@gmail.com>
2021-03-09 23:05:34 +1100

    Partition_List_86

---
+++ src/stack/Partition_List_86.java
@@ -0,0 +1,58 @@
+package stack;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Parition_List_86 {
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(0), dummy2 = new ListNode(0);  //dummy heads of the 1st and 2nd queues
+        ListNode curr1 = dummy1, curr2 = dummy2;      //current tails of the two queues;
+
+        while (head != null) {
+            if (head.val < x) {
+                curr1.next = head;
+                curr1 = head;
+            } else {
+                curr2.next = head;
+                curr2 = head;
+            }
+            head = head.next;
+        }
+
+        curr2.next = null;          //important! avoid cycle in linked list. otherwise u will get TLE.
+        curr1.next = dummy2.next;
+
+        return dummy1.next;
+    }
+
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(0), dummy2 = new ListNode(0);
+        ListNode curr1 = dummy1, curr2 = dummy2;
+        ListNode current = head;
+
+        while (current != head) {
+            if (current.val < x) {
+                curr1.next = current;
+                curr1 = curr1.next;
+                // curr1 = head;
+            } else {
+                curr2.next = current;
+                curr2 = curr2.next;
+                // curr2 = head;
+            }
+
+            current = current.next;
+        }
+        curr2.next = null;
+        curr1.next = dummy2.next;
+
+        return dummy1.next;
+    }
+}
