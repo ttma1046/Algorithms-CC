81a803b7c872035bd1d15e5a12f5060f5908a9aa
Chandler <chandlerfang@gmail.com>
2021-03-10 23:34:48 +1100

    Reorder_List_143, Reverse_Linked_List_II_92

--- src/stack/Partition_List_86.java
+++ src/stack/Partition_List_86.java
@@ -1,4 +1,27 @@
 package stack;
+
+/*
+Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [5], left = 1, right = 1
+Output: [5]
+ 
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= n <= 500
+-500 <= Node.val <= 500
+1 <= left <= right <= n
+*/
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -30,7 +53,6 @@ class Parition_List_86 {
 
         return dummy1.next;
     }
-
 
     public ListNode partition(ListNode head, int x) {
         ListNode dummy1 = new ListNode(0), dummy2 = new ListNode(0);
---
+++ src/stack/Reverse_Linked_List_II_92.java
@@ -0,0 +1,123 @@
+package linkedlist;
+
+/*
+Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [5], left = 1, right = 1
+Output: [5]
+
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= n <= 500
+-500 <= Node.val <= 500
+1 <= left <= right <= n
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Reverse_Linked_List_II_92 {
+	public ListNode reverseBetweenII(ListNode head, int left, int right) {
+		if (head == null) return null;
+
+		ListNode dummy = new ListNode(0); // create a dummy node to mark the head of this list
+		dummy.next = head;
+
+		ListNode pre = dummy; // make a pointer pre as a marker for the node before reversing
+		for (int i = 0; i < left - 1; i++) pre = pre.next;
+
+		ListNode start = pre.next; // a pointer to the beginning of a sub-list that will be reversed
+		ListNode then = start.next; // a pointer to a node that will be reversed
+
+		// 1 - 2 - 3 - 4 - 5 ; m=2; n =4 ---> pre = 1, start = 2, then = 3
+		// dummy-> 1 -> 2 -> 3 -> 4 -> 5
+
+		for (int i = 0; i < right - left; i++) {
+			start.next = then.next;
+			then.next = pre.next;
+			pre.next = then;
+			then = start.next;
+		}
+
+		// first reversing : dummy->1 - 3 - 2 - 4 - 5; pre = 1, start = 2, then = 4
+		// second reversing: dummy->1 - 4 - 3 - 2 - 5; pre = 1, start = 2, then = 5 (finish)
+
+		return dummy.next;
+
+	}
+
+
+	public ListNode reverseBetween(ListNode head, int left, int right) {
+		ListNode dummy = new ListNode(0), pre = dummy;
+		dummy.next = head;
+		
+		for (int i = 0; i < left - 1; i++) pre = pre.next;
+
+		ListNode cur = pre.next;
+
+		/*
+		1 - 2 - 3 - 4 - 5 ; left = 2; right = 4
+
+		pre = 1
+		curr = 2,
+       	temp = 2
+
+       	1 -> 3
+       	2 -> 4
+
+       	1 -> 3 -> 2 -> 4
+
+		temp = 3 -> 2
+       	1 -> 4
+   		2 -> 5
+
+   		1 -> 4 -> 3 -> 2 -> 5
+		*/
+
+		for (int i = 0; i < right - left; i++) {
+			ListNode temp = pre.next;
+			pre.next = cur.next;
+			cur.next = cur.next.next;
+			pre.next.next = temp;
+
+			ListNode abc = dummy.next;
+			while (abc != null) {
+				System.out.println(abc.val);
+				abc = abc.next;
+			}
+
+			System.out.println("========================");
+		}
+
+		return dummy.next;
+	}
+
+	public static void main(String[] args) {
+
+		ListNode five = new ListNode(5), four = new ListNode(4), three = new ListNode(3), two = new ListNode(2), one = new ListNode(1);
+		one.next = two;
+		two.next = three;
+		three.next = four;
+		four.next = five;
+
+		new Reverse_Linked_List_II_92().reverseBetween(one, 2, 4);
+	}
+}
