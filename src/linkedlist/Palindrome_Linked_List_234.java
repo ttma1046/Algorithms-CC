0967789b1e9cdffbcb5bb19623e7ccfe782719d3
Chandler <chandlerfang@gmail.com>
2021-03-11 01:03:56 +1100

    Palindrome_Linked_List_234

---
+++ src/linkedlist/Palindrome_Linked_List_234.java
@@ -0,0 +1,119 @@
+package linkedlist;
+
+/*
+Given the head of a singly linked list, return true if it is a palindrome.
+
+Example 1:
+
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+
+Input: head = [1,2]
+Output: false
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+
+Follow up: Could you do it in O(n) time and O(1) space?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Palindrome_Linked_List_234 {
+	public boolean isPalindrome(ListNode head) {
+		if (head == null) return true;
+
+		ListNode slow = head, fast = head;
+
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+
+		// 1-2-3-4 
+		// 1-2-3-4-5
+		if (fast != null) slow = slow.next;
+
+		ListNode prev = null;
+
+		while (slow != null) {
+			ListNode temp = slow.next;
+			slow.next = prev;
+
+			prev = slow;
+			slow = temp;
+		}
+
+		slow = prev;
+		while(slow != null) {
+			if (head.val != slow.val) return false;
+
+			slow = slow.next;
+			head = head.next;
+		}
+
+		return true;
+	}
+
+	public boolean isPalindromeII(ListNode head) {
+		if (head == null) return true;
+
+		// Find the end of first half and reverse second half.
+		ListNode firstHalfEnd = endOfFirstHalf(head);
+		ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+		// Check whether or not there is a palindrome.
+		ListNode p1 = head;
+		ListNode p2 = secondHalfStart;
+		boolean result = true;
+		while (result && p2 != null) {
+			if (p1.val != p2.val) result = false;
+			p1 = p1.next;
+			p2 = p2.next;
+		}
+
+		// Restore the list and return the result.
+		firstHalfEnd.next = reverseList(secondHalfStart);
+		return result;
+	}
+
+	private ListNode reverseList(ListNode head) {
+		ListNode prev = null;
+		ListNode curr = head;
+		while (curr != null) {
+			ListNode nextTemp = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = nextTemp;
+		}
+		return prev;
+	}
+
+	private ListNode endOfFirstHalf(ListNode head) {
+		ListNode fast = head;
+		ListNode slow = head;
+
+		while (fast.next != null && fast.next.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+
+		return slow;
+	}
+
+	public static void main(String[] args) {
+		new Palindrome_Linked_List_234().isPalindrome(new ListNode(0));
+	}
+}
