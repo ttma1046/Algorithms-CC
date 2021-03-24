9e3be1128d5b3d1822e9c4df6d9d32371c6d9b4f
Chandler <chandlerfang@gmail.com>
2021-03-24 21:41:58 +1100

    Palindrome_order

---
+++ src/hashtable/Palindrome_order.java
@@ -0,0 +1,21 @@
+package hashtable;
+
+/*
+Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+
+ 
+
+Example1:
+
+Input: "tactcoa"
+Output: true（permutations: "tacocat"、"atcocta", etc.）
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/palindrome-permutation-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+class Palindrome_order {
+	public boolean canPermutePalindrome(String s) {
+
+	}
+}
