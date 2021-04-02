8130033d7b605d32635e6021af71a38ea8e161c6
Chandler <chandlerfang@gmail.com>
2021-04-03 01:42:31 +1100

    Quick Sort

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -24,6 +24,9 @@ public class QuickSort {
     }
 
     public static int partition(int[] array, int left, int right, int pivot) {
+        System.out.println("pivot:" + pivot);
+        System.out.println("left:" + left);
+        System.out.println("right:" + right);
         while (left <= right) {
             while (array[left] < pivot) {
                 left++;
@@ -34,7 +37,26 @@ public class QuickSort {
             }
 
             if (left <= right) {
+                System.out.println("left index:" + left + ":value:" + array[left]);
+                System.out.println("right index:" + right + ":value:" + array[right]);
+
+                System.out.println("before swap:");
+                for (int i: array) {
+                    System.out.print(i);
+                    System.out.print(",");
+                }
+                System.out.println();
+
+
                 swap(array, left, right);
+
+                System.out.println("after swap:");
+                for (int i: array) {
+                    System.out.print(i);
+                    System.out.print(",");
+                }
+                System.out.println();
+
                 left++;
                 right--;
             }
@@ -52,5 +74,31 @@ public class QuickSort {
     public static void main(String[] args) {
         sort(new int[] {15, 3, 9, 8, 5, 2, 7, 1, 6});
     }
+
+
+    /*
+
+
+    15 3 9 8 5 2 7 1 6
+    L                R
+    
+
+    5
+
+    15 3 9 8 5 2 7 1 6
+    L              R
+
+    1 3 9 8 5 2 7 15 6
+      L         R
+
+    1 3 9 8 5 2 7 15 6
+        L     R
+
+    1 3 2 8 5 9 7 15 6
+          L R
+
+    1 3 2 5 8 9 7 15 6
+          R L
+    */
 }
 
