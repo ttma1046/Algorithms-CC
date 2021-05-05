a7a62c2f6f944136471aada24b7c36d43e82d072
Chandler <chandlerfang@gmail.com>
2021-05-05 23:32:04 +1000

    Buildings_With_an_Ocean_View_1762

--- src/greedy/Buildings_With_an_Ocean_View_1762.java
+++ src/greedy/Buildings_With_an_Ocean_View_1762.java
@@ -1,5 +1,8 @@
 
-
+package greedy;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 /*
 There are n buildings in a line. You are given an integer array heights of size n that represents the heights of the buildings in the line.
 
@@ -7,7 +10,7 @@ The ocean is to the right of the buildings. A building has an ocean view if the 
 
 Return a list of indices (0-indexed) of buildings that have an ocean view, sorted in increasing order.
 
- 
+
 
 Example 1:
 
@@ -29,10 +32,76 @@ Example 4:
 Input: heights = [2,2,2,2]
 Output: [3]
 Explanation: Buildings cannot see the ocean if there are buildings of the same height to its right.
- 
+
 
 Constraints:
 
 1 <= heights.length <= 105
 1 <= heights[i] <= 109
 */
+
+class Buildings_With_an_Ocean_View_1762 {
+	public int[] findBuildings(int[] heights) {
+		if (heights == null || heights.length == 0) return null;
+
+		int n = heights.length;
+		int top = heights[n - 1];
+
+		List<Integer> ans = new ArrayList<Integer>();
+		ans.add(n - 1);
+
+		for (int i = n - 2; i >= 0; --i) {
+			if (heights[i] > top) {
+				top = heights[i];
+				ans.add(i);
+			}
+		}
+
+        int[] res = new int[ans.size()];
+    	for (int i = ans.size() - 1; i >= 0; --i)
+            res[ans.size() - 1 - i] = ans.get(i);
+
+		return res;
+	}
+
+	public static void main(String[] args) {
+		Buildings_With_an_Ocean_View_1762 result = new Buildings_With_an_Ocean_View_1762();
+
+		int[] heights = new int[] { 4, 2, 3, 1 };
+
+		int[] res = result.findBuildings(heights);
+
+		for (int k : res) {
+			System.out.print(k);
+		}
+
+		System.out.println();
+
+		heights = new int[] {4, 3, 2, 1};
+		res = result.findBuildings(heights);
+
+		for (int k : res) {
+			System.out.print(k);
+		}
+
+		System.out.println();
+
+		heights = new int[] {1, 3, 2, 4};
+		res = result.findBuildings(heights);
+
+		for (int k : res) {
+			System.out.print(k);
+		}
+
+		System.out.println();
+
+		heights = new int[] {2, 2, 2, 2};
+		res = result.findBuildings(heights);
+
+		for (int k : res) {
+			System.out.print(k);
+		}
+
+		System.out.println();
+	}
+}
