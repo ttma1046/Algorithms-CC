51403525968f0fd8bc5de23ddbb7f75172924a44
Chandler <chandlerfang@gmail.com>
2021-05-06 23:27:59 +1000

    Numbers_with_Same_Consecutive_Differences_967

--- src/tree/Numbers_with_Same_Consecutive_Differences_967.java
+++ src/tree/Numbers_with_Same_Consecutive_Differences_967.java
@@ -1,5 +1,9 @@
 package tree;
-
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Arrays;
 /*
 
 Return all non-negative integers of length n such that the absolute difference between every two consecutive digits is k.
@@ -36,7 +40,128 @@ Constraints:
 */
 
 class Numbers_with_Same_Consecutive_Differences_967 {
-    public int[] numsSameConsecDiff(int n, int k) {
-        
-    }
+	public int[] numsSameConsecDiffDFS(int n, int k) {
+		if (n == 1) return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+		List<Integer> results = new ArrayList<Integer>();
+		for (int num = 1; num < 10; ++num) DFS(n - 1, num, k, results);
+
+		int[] res = new int[results.size()];
+		for (int i = 0; i < results.size(); i++) res[i] = results.get(i);
+		return res;
+	}
+
+	private void DFS(int n, int num, int k, List<Integer> results) {
+		if (n == 0) {
+			results.add(num);
+			return;
+		}
+
+		int tail = num % 10;
+		List<Integer> nextDigits = new ArrayList<>();
+
+		if (tail + k >= 0 && tail + k <= 9) nextDigits.add(tail + k);
+		if (k != 0 && tail - k >= 0 && tail - k <= 9) nextDigits.add(tail - k);
+
+		for (int nextDigit : nextDigits) {
+			DFS(n - 1, num * 10 + nextDigit, k, results);
+		}
+	}
+
+
+	public int[] numsSameConsecDiffBFS(int N, int K) {
+		if (N == 1)
+			return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+		List<Integer> queue = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		for (int level = 1; level < N; ++ level) {
+			ArrayList<Integer> nextQueue = new ArrayList<>();
+			// iterate through each number within the level
+			for (Integer num : queue) {
+				Integer tailDigit = num % 10;
+
+				ArrayList<Integer> nextDigits = new ArrayList<>();
+				nextDigits.add(tailDigit + K);
+				if (K != 0)
+					nextDigits.add(tailDigit - K);
+				for (Integer nextDigit : nextDigits) {
+					if (0 <= nextDigit && nextDigit < 10) {
+						Integer newNum = num * 10 + nextDigit;
+						nextQueue.add(newNum);
+					}
+				}
+			}
+			// prepare for the next level
+			queue = nextQueue;
+		}
+
+		int[] res = new int[queue.size()];
+		for (int i = 0; i < queue.size(); i++) res[i] = queue.get(i);
+		return res;
+	}
+
+	public int[] numsSameConsecDiff(int n, int k) {
+		if (n == 1) return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+		Queue<Integer> queue = new LinkedList<>();
+		int size = 0;
+
+		for (int num = 1; num < 10; ++num) queue.offer(num);
+		for (int i = 1; i < n; ++i) {
+			size = queue.size();
+			for (int m = 0; m < size; m++) {
+				int j = queue.poll();
+				int tail = j % 10;
+				if (tail + k >= 0 && tail + k <= 9) queue.offer(j * 10 + tail + k);
+				if (k != 0 && tail - k >= 0 && tail - k <= 9) queue.offer(j * 10 + tail - k);
+			}
+		}
+		
+		size = queue.size();
+		int[] res = new int[size];
+		for (int i = 0; i < size; i++) res[i] = queue.poll();
+		return res;
+	}
+
+	public static void main(String[] args) {
+		Numbers_with_Same_Consecutive_Differences_967 obj = new Numbers_with_Same_Consecutive_Differences_967();
+
+		int[] res = obj.numsSameConsecDiff(3, 7);
+
+		for (int i : res) {
+			System.out.print(i);
+			System.out.print(",");
+		}
+
+		System.out.println();
+
+		res = obj.numsSameConsecDiff(2, 1);
+
+		for (int i : res) {
+			System.out.print(i);
+			System.out.print(",");
+		}
+
+		System.out.println();
+
+		res = obj.numsSameConsecDiff(2, 0);
+
+		for (int i : res) {
+			System.out.print(i);
+			System.out.print(",");
+		}
+
+		System.out.println();
+
+		res = obj.numsSameConsecDiff(2, 2);
+
+		for (int i : res) {
+			System.out.print(i);
+			System.out.print(",");
+		}
+
+		System.out.println();
+	}
+
+
 }
