81e2372f1a09c7cd7a8aefc3641a35d9d270b523
Chandler <chandlerfang@gmail.com>
2021-05-29 21:25:15 +1000

    3Sum_Closest_16

--- src/twopointers/3Sum_Closest_16.java
+++ src/twopointers/3Sum_Closest_16.java
@@ -17,45 +17,43 @@ Constraints:
 -10^3 <= nums[i] <= 10^3
 -10^4 <= target <= 10^4
 */
+
+/*
+Initialize the minimum difference diff with a large value.
+    Sort the input array nums.
+
+    Iterate through the array:
+        For the current position i, set lo to i + 1, and hi to the last index.
+    While the lo pointer is smaller than hi:
+        Set sum to nums[i] + nums[lo] + nums[hi].
+    If the absolute difference between sum and target is smaller than the absolute value of diff:
+    Set diff to target - sum.
+    If sum is less than target, increment lo.
+    Else, decrement hi.
+    If diff is zero, break from the loop.
+    Return the value of the closest triplet, which is target - diff.
+*/
 class ThreeSum_Closest_16 {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
 
-        int n = nums.length;
-
         int diff = Integer.MAX_VALUE;
 
-        for (int i = 0; i < n && diff != 0; i++) {
+        int n = nums.length;
+
+        for(int i = 0; i < n && diff != 0; ++i) {
             int low = i + 1, high = n - 1;
 
             while (low < high) {
                 int sum = nums[i] + nums[low] + nums[high];
-                if (Math.abs(sum - target) < Math.abs(diff)) diff = target - sum;
 
-                if (sum < target)
-                    ++low;
-                else
-                    --high;
+                if (Math.abs(target - sum) < Math.abs(diff)) diff = target - sum;
+
+                if (sum < target) ++low;
+                else --high;
             }
         }
 
-        return target - diff;
-    }
-
-    public int threeSumClosest(int[] nums, int target) {
-        int diff = Integer.MAX_VALUE, sz = nums.length;
-        Arrays.sort(nums);
-        for (int i = 0; i < sz && diff != 0; ++i) {
-            for (int j = i + 1; j < sz - 1; ++j) {
-                int complement = target - nums[i] - nums[j];
-                var idx = Arrays.binarySearch(nums, j + 1, sz - 1, complement);
-                int hi = idx >= 0 ? idx : ~idx, lo = hi - 1;
-                if (hi < sz && Math.abs(complement - nums[hi]) < Math.abs(diff))
-                    diff = complement - nums[hi];
-                if (lo > j && Math.abs(complement - nums[lo]) < Math.abs(diff))
-                    diff = complement - nums[lo];
-            }
-        }
         return target - diff;
     }
 
@@ -67,9 +65,70 @@ class ThreeSum_Closest_16 {
     Space Complexity: from O(logn) to O(n), depending on the implementation of the sorting algorithm.
     */
 
+
+    /*
+
+    Initialize the minimum difference diff with a large value.
+      Sort the input array nums.
+        Iterate through the array (outer loop):
+        For the current position i, iterate through the array starting from j = i + 1 (inner loop):
+        Binary-search for complement (target - nums[i] - nums[j]) in the rest of the array.
+        For the next higher value, check its absolute difference with complement against diff.
+        For the previous lower value, check its absolute difference with complement against diff.
+        Update diff based on the smallest absolute difference.
+        If diff is zero, break from the loop.
+
+        Return the value of the closest triplet, which is target - diff.
+    */
+
+    public int threeSumClosest(int[] nums, int target) {
+        int n = nums.length;
+
+        int diff = Integer.MAX_VALUE;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < sz - 1; ++j) {
+                int complement = target - nums[i] - nums[j];
+
+                int idx = Arrays.binarySearch(nums, j + 1, sz - 1, complement);
+
+                int hi = idx >= 0 ? idx : ~idx, lo = hi - 1;
+                if (hi < sz && Math.abs(complement - nums[hi]) < Math.abs(diff))
+                    diff = complement - nums[hi];
+                if (lo > j && Math.abs(complement - nums[lo]) < Math.abs(diff))
+                    diff = complement - nums[lo];
+            }
+        }
+
+        return target - diff;
+    }
+
     public static void main(String[] args) {
         TreeSum_Closest_16 kk = new ThreeSum_Closest_16();
 
         kk.threeSumClosest(new int[] { -1, 2, 1, -4}, 1);
+    }
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int diff = Integer.MAX_VALUE;
+        int n = nums.length;
+
+        for (int i = 0; i < n && diff != 0; ++i) {
+            int low = i + 1, high = n - 1;
+            while(low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+
+                if (Math.abs(sum - target) < Math.abs(diff)) diff = target - sum;
+
+                if (sum < target) ++low;
+                else --high;
+            }
+        }
+
+        return target - diff;
     }
 }
