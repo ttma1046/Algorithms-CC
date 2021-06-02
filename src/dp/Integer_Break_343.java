82718f5ab9a6836afc12f82e6050e5d2edddfce6
Chandler <chandlerfang@gmail.com>
2021-06-02 20:38:56 +1000

    Integer_Break_343

--- src/dp/Integer_Break_343.java
+++ src/dp/Integer_Break_343.java
@@ -4,7 +4,7 @@ Given an integer n, break it into the sum of k positive integers, where k >= 2, 
 
 Return the maximum product you can get.
 
- 
+
 
 Example 1:
 
@@ -16,7 +16,7 @@ Example 2:
 Input: n = 10
 Output: 36
 Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
- 
+
 
 Constraints:
 
@@ -24,6 +24,29 @@ Constraints:
 */
 class Integer_Break_343 {
     public int integerBreak(int n) {
-        
+        int[] dp = new int[n + 1];
+
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i ++) {
+            for (int j = 1; j < i; j ++) {
+                dp[i] = Math.max(dp[i], (Math.max(j, dp[j])) * (Math.max(i - j, dp[i - j])));
+            }
+        }
+
+        return dp[n];
+    }
+
+    public int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        int product = 1;
+        while (n > 4) {
+            product *= 3;
+            n -= 3;
+        }
+        product *= n;
+
+        return product;
     }
 }
