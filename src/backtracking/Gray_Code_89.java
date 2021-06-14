d1ef692cb5807f41703e6cd4d9c2bb895efca6b8
Chandler Fang <chen.fang@transport.nsw.gov.au>
2021-06-14 15:49:36 +1000

    Gray_Code_89

---
+++ src/backtracking/Gray_Code_89.java
@@ -0,0 +1,57 @@
+package backtracking;
+import java.util.List;
+import java.util.ArrayList;
+/*
+An n-bit gray code sequence is a sequence of 2n integers where:
+
+Every integer is in the inclusive range [0, 2n - 1],
+The first integer is 0,
+An integer appears no more than once in the sequence,
+The binary representation of every pair of adjacent integers differs by exactly one bit, and
+The binary representation of the first and last integers differs by exactly one bit.
+Given an integer n, return any valid n-bit gray code sequence.
+
+Example 1:
+
+Input: n = 2
+Output: [0,1,3,2]
+Explanation:
+The binary representation of [0,1,3,2] is [00,01,11,10].
+- 00 and 01 differ by one bit
+- 01 and 11 differ by one bit
+- 11 and 10 differ by one bit
+- 10 and 00 differ by one bit
+[0,2,3,1] is also a valid gray code sequence, whose binary representation is [00,10,11,01].
+- 00 and 10 differ by one bit
+- 10 and 11 differ by one bit
+- 11 and 01 differ by one bit
+- 01 and 00 differ by one bit
+Example 2:
+
+Input: n = 1
+Output: [0,1]
+
+
+Constraints:
+00 01 11 10
+1 <= n <= 16
+*/
+class Gray_Code_89 {
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        // there are 2 ^ n numbers in the Gray code sequence.
+        int sequenceLength = 1 << n;
+        for (int i = 0; i < sequenceLength; i++) {
+        	
+            int num = i << 1 & i;
+            System.out.println(num);
+            result.add(num);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Gray_Code_89 obj = new Gray_Code_89();
+       	obj.grayCode(2);
+    }
+}
