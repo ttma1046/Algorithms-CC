90b1ff53b810a00ad35fc46807515f7c9b783eea
Chandler Fang <chen.fang@transport.nsw.gov.au>
2021-06-21 23:04:03 +1000

    Find_Minimum_in_Rotated_Sorted_Array_153

--- src/binarysearch/Find_Minimum_in_Rotated_Sorted_Array_153.java
+++ src/binarysearch/Find_Minimum_in_Rotated_Sorted_Array_153.java
@@ -1,5 +1,4 @@
 package binarysearch;
-
 /*
 Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
 
@@ -10,8 +9,6 @@ Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in 
 Given the sorted rotated array nums of unique elements, return the minimum element of this array.
 
 You must write an algorithm that runs in O(log n) time.
-
-
 
 Example 1:
 
@@ -40,49 +37,35 @@ nums is sorted and rotated between 1 and n times.
 */
 class Find_Minimum_in_Rotated_Sorted_Array_153 {
     public int findMin(int[] nums) {
-        // If the list has just one element then return that element.
-        if (nums.length == 1) {
-            return nums[0];
-        }
+        if (nums.length == 1) return nums[0];
 
-        // initializing left and right pointers.
         int left = 0, right = nums.length - 1;
 
-        // if the last element is greater than the first element then there is no rotation.
-        // e.g. 1 < 2 < 3 < 4 < 5 < 7. Already sorted array.
-        // Hence the smallest element is first element. A[0]
-        if (nums[right] > nums[0]) {
-            return nums[0];
-        }
+        if (nums[right] > nums[0]) return nums[0];
 
-        // Binary search way
         while (right >= left) {
-            // Find the mid element
             int mid = left + (right - left) / 2;
 
-            // if the mid element is greater than its next element then mid+1 element is the smallest
-            // This point would be the point of change. From higher to lower value.
-            if (nums[mid] > nums[mid + 1]) {
-                return nums[mid + 1];
-            }
+            if (nums[mid] > nums[mid + 1]) return nums[mid + 1];
 
-            // if the mid element is lesser than its previous element then mid element is the smallest
-            if (nums[mid - 1] > nums[mid]) {
-                return nums[mid];
-            }
+            if (nums[mid - 1] > nums[mid]) return nums[mid];
 
-            // if the mid elements value is greater than the 0th element this means
-            // the least value is still somewhere to the right as we are still dealing with elements
-            // greater than nums[0]
-            if (nums[mid] > nums[0]) {
-                left = mid + 1;
-            } else {
-                // if nums[0] is greater than the mid value then this means the smallest value is somewhere to
-                // the left
-                right = mid - 1;
-            }
+            if (nums[left] < nums[mid]) left = mid + 1;
+            else right = mid - 1;
         }
-        
+
         return -1;
+    }
+
+    public static void main(String[] args) {
+        Find_Minimum_in_Rotated_Sorted_Array_153 obj = new Find_Minimum_in_Rotated_Sorted_Array_153();
+        int[] input = new int[] {3, 4, 5, 1, 2};
+        System.out.println(obj.findMin(input));
+
+        input = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+        System.out.println(obj.findMin(input));
+
+        input = new int[] { 11, 13, 15, 17 };
+        System.out.println(obj.findMin(input));
     }
 }
