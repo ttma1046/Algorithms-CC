5107587344e2355a93526a51e07689742b2eefcf
Chandler Fang <chen.fang@transport.nsw.gov.au>
2021-08-03 22:52:44 +1000

    MeetingRooms_252

--- src/sweepline/MeetingRooms_252.java
+++ src/sweepline/MeetingRooms_252.java
@@ -2,7 +2,24 @@ package sweepline;
 
 import java.util.Arrays;
 import java.util.Comparator;
+/*
+Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
 
+Example 1:
+
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: false
+
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: true
+
+Constraints:
+0 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti < endi <= 106
+*/
 class multiple {
     public int maxGirls(int[][] input) {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
@@ -84,6 +101,15 @@ class MeetingRooms_252 {
 
         Space complexity : O(1). Since no additional space is allocated.
     */
+    public boolean canAttendMettingsMy(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) return true;
+
+        Arrays.sort(intervals, (int[] a, int[] b) -> a[0] - b[0]);
+
+        for (int i = 1; i < intervals.length; ++i) if (intervals[i - 1][1] > intervals[i][0]) return false;
+
+        return true;
+    }
 
     public static void main(String[] args) {
         System.out.println(new MeetingRooms_252().canAttendMettingsIII(new int[][] {{ 7, 10 }, { 2, 4 }}));
