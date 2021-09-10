15a66c1b416a726f5be6a614f72522d6fabb0b72
Chandler Fang <chen.fang@transport.nsw.gov.au>
2021-09-10 22:11:44 +1000

    Serialize_and_Deserialize_BST_449 ConstructBinarySearchTreefromPreOrderTraversal_1008

--- src/tree/Serialize_and_Deserialize_BST_449.java
+++ src/tree/Serialize_and_Deserialize_BST_449.java
@@ -1,18 +1,22 @@
 package tree;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Arrays;
+
 /*
-Serialization is converting a data structure or object 
+Serialization is converting a data structure or object
 
-into a sequence of bits so that it can be stored in a file or memory buffer, 
+into a sequence of bits so that it can be stored in a file or memory buffer,
 
-or transmitted across a network connection link to be reconstructed 
+or transmitted across a network connection link to be reconstructed
 
 later in the same or another computer environment.
 
-Design an algorithm to serialize and deserialize a binary search tree. 
+Design an algorithm to serialize and deserialize a binary search tree.
 
-There is no restriction on how your serialization/deserialization algorithm should work. 
+There is no restriction on how your serialization/deserialization algorithm should work.
 
-You need to ensure that a binary search tree can be serialized to a string, 
+You need to ensure that a binary search tree can be serialized to a string,
 
 and this string can be deserialized to the original tree structure.
 
@@ -38,23 +42,76 @@ The input tree is guaranteed to be a binary search tree.
 /**
  * Definition for a binary tree node.
  */
- public class TreeNode {
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int x) { val = x; }
- }
- 
-public class Codec {
+    TreeNode(int x) {
+        val = x;
+    }
+}
 
-    // Encodes a tree to a single string.
+public class Serialize_and_Deserialize_BST_449 {
+    public static void main(String[] args) {
+        Codec test = new Codec();
+
+        TreeNode two = new TreeNode(2);
+        TreeNode three = new TreeNode(3);
+
+        TreeNode one = new TreeNode(1);
+        one.left = two;
+        one.right = three;
+
+        String res = test.serialize(one);
+        System.out.println(res);
+
+        TreeNode node = test.deserialize(res);
+    }
+}
+
+class Codec {
     public String serialize(TreeNode root) {
-        
+        StringBuilder sb = new StringBuilder();
+        serialize(root, sb);
+
+        return sb.toString();
+    }
+
+    private void serialize(TreeNode root, StringBuilder sb) {
+        if (root == null) return;
+
+        sb.append(String.valueOf(root.val)).append(",");
+
+        if (root.left != null) serialize(root.left, sb);
+        if (root.right != null) serialize(root.right, sb);
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        
+        if (data.length() == 0) return null;
+
+        Queue<String> q = new LinkedList<>(Arrays.asList(data.split(",")));
+
+        return deserialize(q, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private TreeNode deserialize(Queue<String> q, int lower, int upper) {
+        if (q.size() == 0) return null;
+
+        String curr = q.peek();
+
+        int value = Integer.parseInt(curr);
+
+        if (value < lower ||  value > upper) return null;
+
+        q.poll();
+
+        TreeNode node = new TreeNode(value);
+
+        node.left = deserialize(q, lower, value);
+        node.right = deserialize(q, value, upper);
+
+        return node;
     }
 }
 
