a2c3c0a53f06e41d6f89f19d4f79ed246817025b
Chandler Fang <chen.fang@transport.nsw.gov.au>
2021-10-06 14:41:50 +1100

    Intersection_of_two_linked_lists_160

--- src/linkedlist/Intersection_of_two_linked_lists_160.java
+++ src/linkedlist/Intersection_of_two_linked_lists_160.java
@@ -3,6 +3,50 @@ import java.util.Set;
 import java.util.HashSet;
 
 /*
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. 
+
+If the two linked lists have no intersection at all, return null.
+
+For example, the following two linked lists begin to intersect at node ```c1```:
+
+The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+
+Note that the linked lists must retain their original structure after the function returns.
+
+Custom Judge:
+
+The inputs to the judge are given as follows (your program is not given these inputs):
+
+intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+listA - The first linked list.
+listB - The second linked list.
+skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
+
+Example 1:
+
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+Output: Intersected at '8'
+Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+
+Example 2:
+
+Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+Output: Intersected at '2'
+Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+
+Example 3:
+
+Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+Output: No intersection
+Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+Explanation: The two lists do not intersect, so return null.
+*/
+
+/*
 Given the heads of two singly linked-lists headA and headB,
 
 return the node at which the two lists intersect.
@@ -40,16 +84,18 @@ Constraints:
 
 The number of nodes of listA is in the m.
 The number of nodes of listB is in the n.
-1 <= m, n <= 3 * 104
+0 <= m, n <= 3 * 104
 1 <= Node.val <= 105
-1 <= skipA <= m
-1 <= skipB <= n
+0 <= skipA <= m
+0 <= skipB <= n
 intersectVal is 0 if listA and listB do not intersect.
-intersectVal == listA[skipA + 1] == listB[skipB + 1] if listA and listB intersect.
+intersectVal == listA[skipA] == listB[skipB] if listA and listB intersect.
+ 
+
+Follow up: Could you write a solution that runs in O(n) time and use only O(1) memory?
 */
 
 class Intersection_of_two_linked_lists_160 {
-
 	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 		Set<ListNode> set = new HashSet<>();
 
@@ -87,9 +133,9 @@ class Intersection_of_two_linked_lists_160 {
 		ListNode pA = headA;
 		ListNode pB = headB;
 
-		while (pA != pB) {
-			pA = pA == null ? headB : a.next;
-			pB = pB == null ? headA : b.next;
+		while(pA.val != pB.val) {
+			pA = pA == null ? headB : pA.next;
+			pB = pB == null ? headA : pB.next;
 		}
 
 		return pA;
@@ -106,11 +152,9 @@ class Intersection_of_two_linked_lists_160 {
 
 	We aren't allocating any additional data structures, so the amount of extra space used does not grow with the size of the input. For this reason, Approach 3 is better than Approach 2.
 	*/
-
-	
 	public static void main(String[] args) {
-		// new Intersection_of_two_linked_lists_160().getIntersectionNode();
+		Intersection_of_two_linked_lists_160 obj = new Intersection_of_two_linked_lists_160();
 
-
+		obj.getIntersectionNode();
 	}
 }
