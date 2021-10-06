a9aee966a9547d2bdd3645ffd67d0cbf3d14600e
Chandler Fang <chen.fang@transport.nsw.gov.au>
2021-10-06 14:42:45 +1100

    Palindrome_Linked_List_234

--- src/linkedlist/Palindrome_Linked_List_234.java
+++ src/linkedlist/Palindrome_Linked_List_234.java
@@ -23,97 +23,155 @@ Follow up: Could you do it in O(n) time and O(1) space?
 
 /**
  * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
  */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+/**/
 class Palindrome_Linked_List_234 {
-	public boolean isPalindrome(ListNode head) {
-		if (head == null) return true;
+    public boolean isPalindrome(ListNode head) {
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverse(firstHalfEnd.next);
 
-		ListNode slow = head, fast = head;
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
 
-		while(fast != null && fast.next != null) {
-			slow = slow.next;
-			fast = fast.next.next;
-		}
+        boolean result = true;
 
-		// 1-2-3-4 
-		// 1-2-3-4-5
-		if (fast != null) slow = slow.next;
+        while(result && p2 != null) {
+            if (p1.val != p2.val) result = false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
 
-		ListNode prev = null;
+        firstHalfEnd.next = reverse(secondHalfStart);
+        return result;
+    }
 
-		while (slow != null) {
-			ListNode temp = slow.next;
-			slow.next = prev;
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
 
-			prev = slow;
-			slow = temp;
-		}
+        while(fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
 
-		slow = prev;
-		while(slow != null) {
-			if (head.val != slow.val) return false;
+        return slow;
+    }
 
-			slow = slow.next;
-			head = head.next;
-		}
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while(head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
 
-		return true;
-	}
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) return true;
 
-	public boolean isPalindromeII(ListNode head) {
-		if (head == null) return true;
+        ListNode slow = head, fast = head;
 
-		// Find the end of first half and reverse second half.
-		ListNode firstHalfEnd = endOfFirstHalf(head);
-		ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
 
-		// Check whether or not there is a palindrome.
-		ListNode p1 = head;
-		ListNode p2 = secondHalfStart;
-		boolean result = true;
-		while (result && p2 != null) {
-			if (p1.val != p2.val) result = false;
-			p1 = p1.next;
-			p2 = p2.next;
-		}
+        // 1-2-3-4
+        // 1-2-3-4-5
+        if (fast != null) slow = slow.next;
 
-		// Restore the list and return the result.
-		firstHalfEnd.next = reverseList(secondHalfStart);
-		return result;
-	}
+        System.out.println(slow.val);
 
-	private ListNode reverseList(ListNode head) {
-		ListNode prev = null;
-		ListNode curr = head;
-		while (curr != null) {
-			ListNode nextTemp = curr.next;
-			curr.next = prev;
-			prev = curr;
-			curr = nextTemp;
-		}
-		return prev;
-	}
+        ListNode prev = null;
 
-	private ListNode endOfFirstHalf(ListNode head) {
-		ListNode fast = head;
-		ListNode slow = head;
+        while (slow != null) {
+            ListNode temp = slow.next;
+            slow.next = prev;
 
-		while (fast.next != null && fast.next.next != null) {
-			fast = fast.next.next;
-			slow = slow.next;
-		}
+            prev = slow;
+            slow = temp;
+        }
 
-		return slow;
-	}
+        slow = prev;
+        while(slow != null) {
+            if (head.val != slow.val) return false;
 
-	public static void main(String[] args) {
-		new Palindrome_Linked_List_234().isPalindrome(new ListNode(0));
-	}
+            slow = slow.next;
+            head = head.next;
+        }
+
+        return true;
+    }
+
+
+
+    public boolean isPalindromeII(ListNode head) {
+        if (head == null) return true;
+
+        // Find the end of first half and reverse second half.
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+        // Check whether or not there is a palindrome.
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) result = false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        // Restore the list and return the result.
+        firstHalfEnd.next = reverseList(secondHalfStart);
+        return result;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        Palindrome_Linked_List_234 obj = new Palindrome_Linked_List_234();
+
+        obj.isPalindrome(
+        	new ListNode(1, 
+        		new ListNode(2, 
+        			new ListNode(3, 
+        				new ListNode(4)))));
+    }
 }
