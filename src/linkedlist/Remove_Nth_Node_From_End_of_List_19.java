6371ae9c3a0f720c2fa5a1dbba94e60a34dd70be
Chandler Fang <chen.fang@transport.nsw.gov.au>
2021-10-06 14:43:58 +1100

    Remove_Nth_Node_From_End_of_List_19

--- src/linkedlist/Remove_Nth_Node_From_End_of_List_19.java
+++ src/linkedlist/Remove_Nth_Node_From_End_of_List_19.java
@@ -1,25 +1,84 @@
+package linkedlist;
+
+/*
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+Constraints:
+
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+
+Follow up: Could you do this in one pass?
+*/
 class Remove_Nth_Node_From_End_of_List_19 {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null) return null;
-        if (n == 0) return head;
+        /*
+        if(head.next == null) return null;
 
-        ListNode temp = new ListNode();
+        ListNode dummy = new ListNode(-1);
 
-        ListNode slow = temp;
-        ListNode fast = temp;
+        dummy.next = head;
 
-        while ((n + 1)-- > 0) {
-            fast = fast.next;
-        }
+        ListNode slow = dummy, fast = dummy;
 
-        while (fast != null) {
+        for(int i = 0; i < n; ++i) fast = fast.next;
+
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next;
         }
 
-        ListNode temp = slow.next;
         slow.next = slow.next.next;
-        temp = null;
-        return head;
+
+        return dummy.next;
+        */
+        if(head.next == null) return null;
+
+        ListNode dummy = new ListNode(-1);
+
+        dummy.next = head;
+        
+        ListNode slow = dummy, fast = dummy;
+
+        for(int i = 0; i < n + 1; ++i) fast = fast.next;
+
+        while(fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode one =
+            new ListNode(1,
+                         new ListNode(2,
+                                      new ListNode(3,
+                                              new ListNode(4,
+                                                      new ListNode(5)))));
+
+        Remove_Nth_Node_From_End_of_List_19 obj = new Remove_Nth_Node_From_End_of_List_19();
+
+        obj.removeNthFromEnd(one, 2);
     }
 }
