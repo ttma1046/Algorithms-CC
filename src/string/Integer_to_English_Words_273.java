616bc4588f7275e00495f67a76330c3933d990a0
Chandler Fang <chen.fang@transport.nsw.gov.au>
2021-10-29 21:05:02 +1100

    Integer_to_English_Words_273

--- src/string/Integer_to_English_Words_273.java
+++ src/string/Integer_to_English_Words_273.java
@@ -100,48 +100,48 @@ class Solution {
 
 class Solution {
     final String[] LESS_THAN_20 = {
-        "", 
-        "One", 
-        "Two", 
-        "Three", 
-        "Four", 
-        "Five", 
-        "Six", 
-        "Seven", 
-        "Eight", 
-        "Nine", 
-        "Ten", 
-        "Eleven", 
-        "Twelve", 
-        "Thirteen", 
-        "Fourteen", 
-        "Fifteen", 
-        "Sixteen", 
-        "Seventeen", 
-        "Eighteen", 
+        "",
+        "One",
+        "Two",
+        "Three",
+        "Four",
+        "Five",
+        "Six",
+        "Seven",
+        "Eight",
+        "Nine",
+        "Ten",
+        "Eleven",
+        "Twelve",
+        "Thirteen",
+        "Fourteen",
+        "Fifteen",
+        "Sixteen",
+        "Seventeen",
+        "Eighteen",
         "Nineteen"
     };
-    
+
     final String[] TENS = {
-        "", 
-        "Ten", 
-        "Twenty", 
-        "Thirty", 
-        "Forty", 
-        "Fifty", 
-        "Sixty", 
-        "Seventy", 
-        "Eighty", 
+        "",
+        "Ten",
+        "Twenty",
+        "Thirty",
+        "Forty",
+        "Fifty",
+        "Sixty",
+        "Seventy",
+        "Eighty",
         "Ninety"
     };
 
     final String[] THOUSANDS = {
-        "Billion", 
-        "Million", 
-        "Thousand", 
+        "Billion",
+        "Million",
+        "Thousand",
         ""
     };
-    
+
     final int[] radix = {1000000000, 1000000, 1000, 1};
 
     public String numberToWords(int num) {
@@ -149,7 +149,7 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < radix.length; i++) {
             if (num / radix[i] == 0) continue;
-            sb.append(trans(num / radix[i])).append(THOUSANDS[i]).append(' ');
+            sb.append(trans(num / radix[i])).append(THOUSANDS[i]).append(" ");
             num %= radix[i];
         }
         return sb.toString().trim();
@@ -160,5 +160,68 @@ class Solution {
         if (num < 20) return LESS_THAN_20[num] + " ";
         if (num < 100) return TENS[num / 10] + " " + trans(num % 10);
         return LESS_THAN_20[num / 100] + " Hundred " + trans(num % 100);
+    }
+    
+    final String[] LESS_THAN_20 = {
+        "One",
+        "Two",
+        "Three",
+        "Four",
+        "Five",
+        "Six",
+        "Seven",
+        "Eight",
+        "Nine",
+        "Ten",
+        "Eleven",
+        "Twelve",
+        "Thirteen",
+        "Fourteen",
+        "Fifteen",
+        "Sixteen",
+        "Seventeen",
+        "Eighteen",
+        "Nineteen",
+    };
+
+    final String[] TENS = {
+        "Twenty",
+        "Thirty",
+        "Forty",
+        "Fifty",
+        "Sixty",
+        "Seventy",
+        "Eighty",
+        "Ninety"
+    };
+
+    final String[] THOUSANDS = {
+        "Billion",
+        "Million",
+        "Thousand",
+        ""
+    };
+
+    final int[] radix = {1000000000, 1000000, 1000, 1}
+
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < radix.length; ++i) {
+            if ((num / radix[i]) == 0) continue;
+            sb.append(helper(num / radix[i])).append(THOUSANDS[i]).append(" ");
+            num %= radix[i];
+        }
+
+        sb.toString().trim();
+    }
+
+    private String helper(int i) {
+        if (i == 0) return "";
+        if (i < 20) return LESS_THAN_20[i];
+        if (i < 100) return TENS[i / 10] + " " + helper(i % 10);
+        return LESS_THAN_20[i / 100] + " Hundred " + helper(i % 100);
     }
 }
