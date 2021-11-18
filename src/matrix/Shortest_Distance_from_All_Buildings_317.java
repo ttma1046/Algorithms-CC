ef099c856250277b7845439bd96590511cf96518
Chandler <chandlerfang@gmail.com>
2021-11-18 21:16:12 +1100

    Shortest_Distance_from_All_Buildings_317

--- src/matrix/Shortest_Distance_from_All_Buildings_317.java
+++ src/matrix/Shortest_Distance_from_All_Buildings_317.java
@@ -42,7 +42,50 @@ n == grid[i].length
 grid[i][j] is either 0, 1, or 2.
 There will be at least one building in the grid.
 */
+
 class Solution {
+    public int shortestDistance(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+
+
+        
+    }
+}
+
+
+class Solution {
+    public int shortestDistance(int[][] grid) {
+        int minDistance = Integer.MAX_VALUE;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int totalHouses = 0;
+
+        for (int row = 0; row < rows; ++row) {
+            for (int col = 0; col < cols; ++col) {
+                if (grid[row][col] == 1) {
+                    totalHouses++;
+                }
+            }
+        }
+
+        // Find the min distance sum for each empty cell.
+        for (int row = 0; row < rows; ++row) {
+            for (int col = 0; col < cols; ++col) {
+                if (grid[row][col] == 0) {
+                    minDistance = Math.min(minDistance, bfs(grid, row, col, totalHouses));
+                }
+            }
+        }
+
+        // If it is impossible to reach all houses from any empty cell, then return -1.
+        if (minDistance == Integer.MAX_VALUE) {
+            return -1;
+        }
+
+        return minDistance;
+    }
+
     private int bfs(int[][] grid, int row, int col, int totalHouses) {
         // Next four directions.
         int dirs[][] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
@@ -108,37 +151,6 @@ class Solution {
         // If we have reached all houses then return the total distance calculated.
         return distanceSum;
     }
-
-    public int shortestDistance(int[][] grid) {
-        int minDistance = Integer.MAX_VALUE;
-        int rows = grid.length;
-        int cols = grid[0].length;
-        int totalHouses = 0;
-
-        for (int row = 0; row < rows; ++row) {
-            for (int col = 0; col < cols; ++col) {
-                if (grid[row][col] == 1) {
-                    totalHouses++;
-                }
-            }
-        }
-
-        // Find the min distance sum for each empty cell.
-        for (int row = 0; row < rows; ++row) {
-            for (int col = 0; col < cols; ++col) {
-                if (grid[row][col] == 0) {
-                    minDistance = Math.min(minDistance, bfs(grid, row, col, totalHouses));
-                }
-            }
-        }
-
-        // If it is impossible to reach all houses from any empty cell, then return -1.
-        if (minDistance == Integer.MAX_VALUE) {
-            return -1;
-        }
-
-        return minDistance;
-    }
 }
 /*
 Let N and M be the number of rows and columns in grid respectively.
@@ -159,7 +171,6 @@ Space Complexity: O(N⋅M)
 
 We use an extra matrix to track the visited cells, and the queue will store each matrix element at most once during each BFS call. Hence, O(N⋅M) space is required.
 */
-
 
 class Solution {
     private void bfs(int[][] grid, int[][][] distances, int row, int col) {
@@ -246,7 +257,33 @@ class Solution {
     }
 };
 
+/*
+Complexity Analysis
 
+Let NN and MM be the number of rows and columns in grid respectively.
+
+Time Complexity: O(N^2 * M^2)
+
+For each house, we will traverse across all reachable land.
+
+This will require O(number of zeros * number of ones) time and the number of zeros
+
+and ones in the matrix is of order N * M.
+
+Consider that when half of the values in grid are 0 and half of the values are 1,
+
+total elements in grid will be (M * N) so their counts are (M * N) / 2 and (M * N)/2 respectively,
+
+thus giving time complexity (M * N)/2 * (M * N)/2, which results in O(N^2 * M^2).
+
+Space Complexity: O(N * M)
+
+We use an extra matrix to track the visited cells and another one to store distance sum
+
+along with the house counter for each empty cell, and the queue will store each matrix element at most once during each BFS call.
+
+Hence, O(N * M) space is required.
+*/
 class Solution {
     public int shortestDistance(int[][] grid) {
         // Next four directions.
@@ -306,5 +343,170 @@ class Solution {
         }
 
         return minDist == Integer.MAX_VALUE ? -1 : minDist;
+    }
+}
+/*
+Complexity Analysis
+
+Let N and M be the number of rows and columns in grid respectively.
+
+Time Complexity: O(N^2 * M^2)
+
+For each house, we will traverse across all reachable land.
+
+This will require O(number of zeros * number of ones) time and the number of zeros and ones
+
+in the matrix is of order N * M.
+
+Consider that when half of the values in grid are 0 and half of the values are 1,
+
+total elements in grid would be N * M
+
+so their counts are (M * N)/2 and (M * N)/2 respectively,
+
+thus giving time complexity (M * N)/2 * (M * N)/2, that results in O(N^2 * M^2).
+
+Space Complexity: O(N * M)
+
+We use an extra matrix to store distance sums, and the queue will store each matrix element at most once during each BFS call. Hence, O(N * M) space is required.
+*/
+
+class Solution {
+    public int shortestDistance(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        List<tuple> buildings = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1)
+                    buildings.add(new tuple(i, j, 0));
+
+                grid[i][j] = -grid[i][j];
+            }
+        }
+
+        int[][] result = new int[m][n];
+
+        for (int i = 0; i < buildings.size(); i++)
+            bfs(buildings.get(i), i, grid, result);
+
+        int shortest = -1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == buildings.size()
+                        && (shortest == -1 || shortest > result[i][j]))
+                    shortest = result[i][j];
+            }
+        }
+
+        return shortest;
+    }
+
+    private void bfs(tuple cur, int k, int[][] grid, int[][] result) {
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        Queue<tuple> q = new ArrayDeque<>();
+        q.add(cur);
+        while (!q.isEmpty()) {
+            cur = q.poll();
+            int x = cur.x, y = cur.y, dist = cur.dist;
+            for (int i = 0; i < 4; i++) {
+                int newX = x + dirs[i][0];
+                int newY = y + dirs[i][1];
+                if (newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length
+                        && grid[newX][newY] == k) {
+                    grid[newX][newY] = k + 1;
+                    result[newX][newY] += dist + 1;
+                    q.offer(new tuple(newX, newY, dist + 1));
+                }
+            }
+        }
+    }
+
+    class tuple {
+        int row;
+        int col;
+        int dist;
+        tuple(int i, int j, int c) {
+            row = i;
+            col = j;
+            dist = c;
+        }
+    }
+}
+
+/*
+Inspired by previous solution.
+The main idea is the following:
+
+Traverse the matrix. For each building, use BFS to compute the shortest distance from each '0' to
+this building. After we do this for all the buildings, we can get the sum of shortest distance
+from every '0' to all reachable buildings. This value is stored
+in 'distance[][]'. For example, if grid[2][2] == 0, distance[2][2] is the sum of shortest distance from this block to all reachable buildings.
+Time complexity: O(number of 1)O(number of 0) ~ O(m^2n^2)
+
+We also count how many building each '0' can be reached. It is stored in reach[][]. This can be done during the BFS. We also need to count how many total buildings are there in the matrix, which is stored in 'buildingNum'.
+
+Finally, we can traverse the distance[][] matrix to get the point having shortest distance to all buildings. O(m*n)
+
+The total time complexity will be O(m^2*n^2), which is quite high!. Please let me know if I did the analysis wrong or you have better solution.
+*/
+public class Solution {
+    public int shortestDistance(int[][] grid) {
+        if (grid == null || grid[0].length == 0) return 0;
+        final int[] shift = new int[] {0, 1, 0, -1, 0};
+
+        int row  = grid.length, col = grid[0].length;
+        int[][] distance = new int[row][col];
+        int[][] reach = new int[row][col];
+        int buildingNum = 0;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    buildingNum++;
+                    Queue<int[]> myQueue = new LinkedList<int[]>();
+                    myQueue.offer(new int[] {i, j});
+
+                    boolean[][] isVisited = new boolean[row][col];
+                    int level = 1;
+
+                    while (!myQueue.isEmpty()) {
+                        int qSize = myQueue.size();
+                        for (int q = 0; q < qSize; q++) {
+                            int[] curr = myQueue.poll();
+
+                            for (int k = 0; k < 4; k++) {
+                                int nextRow = curr[0] + shift[k];
+                                int nextCol = curr[1] + shift[k + 1];
+
+                                if (nextRow >= 0 && nextRow < row && nextCol >= 0 && nextCol < col
+                                        && grid[nextRow][nextCol] == 0 && !isVisited[nextRow][nextCol]) {
+                                    //The shortest distance from [nextRow][nextCol] to thic building
+                                    // is 'level'.
+                                    distance[nextRow][nextCol] += level;
+                                    reach[nextRow][nextCol]++;
+
+                                    isVisited[nextRow][nextCol] = true;
+                                    myQueue.offer(new int[] {nextRow, nextCol});
+                                }
+                            }
+                        }
+                        level++;
+                    }
+                }
+            }
+        }
+
+        int shortest = Integer.MAX_VALUE;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 0 && reach[i][j] == buildingNum) {
+                    shortest = Math.min(shortest, distance[i][j]);
+                }
+            }
+        }
+
+        return shortest == Integer.MAX_VALUE ? -1 : shortest;
     }
 }
