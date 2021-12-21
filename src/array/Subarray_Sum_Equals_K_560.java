24b80b7778e4ca5aab5308db74832969be4eb7c4
Chandler <chandlerfang@gmail.com>
2021-12-21 22:39:01 +1100

    Subarray_Sum_Equals_K_560

--- src/array/Subarray_Sum_Equals_K_560.java
+++ src/array/Subarray_Sum_Equals_K_560.java
@@ -4,24 +4,19 @@ import java.util.HashMap;
 
 class Subarray_Sum_Equals_K_560 {
     public int subarraySumIII(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
+        if (nums == null || nums.length == 0) return 0;        
 
         int result = 0;
         
         for(int i = 0; i < nums.length; i++) {
             int sum = nums[i];
 
-            if (sum == k) {
-                result++;
-            }
+            if (sum == k) result++;
+            
 
             for (int j = i + 1; j < nums.length; j++) {
-                if (sum + nums[j] == k) {
-                    result++;
-                }
-
+                if (sum + nums[j] == k) result++;
+                
                 sum += nums[j];
             }
         }
@@ -31,31 +26,27 @@ class Subarray_Sum_Equals_K_560 {
 
 
     public int subarraySumII(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
+        if (nums == null || nums.length == 0) return 0;        
 
         int result = 0;
         int sum = 0;
 
-        HashMap<Integer, Integer> myHash = new HashMap<Integer, Integer>();
-        myHash.put(0, 1); // super-duper
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(0, 1); // super-duper
 
         for (int i = 0; i < nums.length; i++) {
             sum +=  nums[i];
-            if (myHash.containsKey(sum - k)) {
-                result += map.get(sum - k);
-            }
+            if (map.containsKey(sum - k)) result += map.get(sum - k);            
 
-            myHash.put(sum, myHash.getOrDefault(sum, 0)++);
+            map.put(sum, map.getOrDefault(sum, 0)++);
         }
 
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Subarray_Sum_Equals_K_560().subarraySum(new int[] { 28, 54, 7, -70, 22, 65, -6 }, 100));
+        System.out.println(new Subarray_Sum_Equals_K_560().subarraySumII(new int[] { 28, 54, 7, -70, 22, 65, -6 }, 100));
 
-        System.out.println(new Subarray_Sum_Equals_K_560().subarraySum(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 0));
+        System.out.println(new Subarray_Sum_Equals_K_560().subarraySumII(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 0));
     }
 }
