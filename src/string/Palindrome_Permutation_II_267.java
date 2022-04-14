b4fd7af78187cdfbb858b862bcc207edf4616f55
Chandler Fang <chen.fang@transport.nsw.gov.au>
2022-04-15 03:12:38 +1000

    Shortest_Path_in_Binary_Matrix_1091 Palindrome_Permutation_II_267

--- src/string/Palindrome_Permutation_II_267.java
+++ src/string/Palindrome_Permutation_II_267.java
@@ -1,4 +1,8 @@
 package string;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 /*
 Given a string s, return all the palindromic permutations (without duplicates) of it.
 
@@ -24,61 +28,84 @@ public class Palindrome_Permutation_II_267 {
     Set<String> set = new HashSet<>();
 
     public List<String> generatePalindromes(String s) {
-        int[] map = new int[28];
+        // int[] map = new int[26];
+        int[] map = new int[128];
+        if (!isPalindromes(s, map))
+            return new ArrayList<String>();
 
-        char[] st = new char[s.length() / 2];
-        
-        if (!canPermutePalindrome(s, map))
-            return new ArrayList<>();
-        
-        char ch = '~';
-
+        char ch = 0;
         int k = 0;
-        
-        for (int i = 0; i < map.length; i++) {
-            if (map[i] % 2 == 1)
-                ch = (char)i;
+        char[] d = new char[s.length() / 2];
 
+        for (int i = 0; i < map.length; ++i) {
+            /*
+            if (map[i] % 2 == 1)
+                ch = (char)('a' + i);
+
+            for (int j = 0; j < map[i] / 2; ++j)
+                d[k++] = (char)('a' + i);
+            */
+
+            if (map[i] % 2 == 1)
+                ch = (char) i;
             for (int j = 0; j < map[i] / 2; j++)
-                st[k++] = (char)(i + 'a');
+                d[k++] = (char) i;
         }
-        
-        permute(st, 0, ch);
+
+        premute(d, 0, ch);
+
         return new ArrayList<String>(set);
     }
-    
-    public boolean canPermutePalindrome(String s, int[] map) {
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            map[s.charAt(i) - 'a']++;
 
-            if (map[s.charAt(i) - 'a'] % 2 == 0)
+    private void swap(char[] s, int l, int i) {
+        char temp = s[l];
+        s[l] = s[i];
+        s[i] = temp;
+    }
+
+    private void premute(char[] chars, int l, char ch) {
+        if (l == chars.length) {
+            set.add(new String(chars) + (ch == 0 ? "" : ch) + new StringBuffer(new String(chars)).reverse());
+        } else {
+            for (int i = l; i < chars.length; ++i) {
+                if (chars[l] != chars[i] || l == i) {
+                    swap(chars, l, i);
+                    premute(chars, l + 1, ch);
+                    swap(chars, l, i);
+                }
+            }
+        }
+    }
+
+    private boolean isPalindromes(String s, int[] map) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); ++i) {
+            // map[s.charAt(i) - 'a']++;
+            map[s.charAt(i)]++;
+
+            // if (map[s.charAt(i) - 'a'] % 2 == 0)
+            if (map[s.charAt(i)] % 2 == 0)
                 count--;
             else
                 count++;
         }
+
         return count <= 1;
     }
-    
-    public void swap(char[] s, int i, int j) {
-        char temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-    }
 
-    void permute(char[] s, int l, char ch) {
-        if (l == s.length) {
-            set.add(
-            	new String(s) + (ch == 0 ? "" : ch) + new StringBuffer(new String(s)).reverse()
-            );
-        } else {
-            for (int i = l; i < s.length; i++) {
-                if (s[l] != s[i] || l == i) {
-                    swap(s, l, i);
-                    permute(s, l + 1, ch);
-                    swap(s, l, i);
-                }
-            }
-        }
+    /*
+    Time complexity : O((n/2 + 1)!). Atmost n/2! permutations need to be generated in the worst case. 
+
+    Further, for each permutation generated, string.reverse() function will take n/4 time.
+
+    Space complexity: O(n). The depth of recursion tree can go upto n/2 in the worst case.
+    */
+
+    public static void main(String[] args) {
+        Palindrome_Permutation_II_267 obj = new Palindrome_Permutation_II_267();
+        List<String> res = obj.generatePalindromes("aabb");
+        for (String temp: res) 
+            System.out.println(temp);
     }
 }
