fb9961e0a2c33095c39852bd60f2d34c81ee6ebe
Chandler Fang <chen.fang@transport.nsw.gov.au>
2022-07-10 16:37:04 +1000

    Unique_Binary_Search_Trees_II_95

--- src/tree/Unique_Binary_Search_Trees_II_95.java
+++ src/tree/Unique_Binary_Search_Trees_II_95.java
@@ -73,37 +73,6 @@ class Unique_Binary_Search_Trees_II_95 {
         return res;
     }
 
-    public List<TreeNode> generateTrees(int n) {
-        if (n == 0) 
-            return new ArrayList<TreeNode>();
-        return helper(1, n);
-    }
-
-    public List<TreeNode> helper(int start, int end) {
-        List<TreeNode> res = new ArrayList<>();
-
-        if (start > end) {
-            res.add(null);
-            return res;
-        }
-
-        for (int i = start; i <= end; i++) {
-            List<TreeNode> leftList = helper(start, i - 1);
-            List<TreeNode> rightList = helper(i + 1, end);
-
-            for (TreeNode left : leftList) {
-                for (TreeNode right : rightList) {
-                    TreeNode root = new TreeNode(i);
-                    root.left = left;
-                    root.right = right;
-                    res.add(root);
-                }
-            }
-        }
-
-        return res;
-    }
-
     public static void main(String[] args) {
         Unique_Binary_Search_Trees_II_95 obj = new Unique_Binary_Search_Trees_II_95();
     }
